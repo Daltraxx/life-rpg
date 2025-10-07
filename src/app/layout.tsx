bda5dc752d12e49be14e7284ba27e49a9c9a5a38
Dalton Pettus <daltpettus@gmail.com>
2025-10-07 14:30:32 -0700

    import jersey font and make variable available for use

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { jersey } from "./ui/fonts";
 import "./globals.css";
+import clsx from "clsx";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +27,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={clsx(
+          jersey.variable,
+          geistSans.variable,
+          geistMono.variable,
+          "antialiased"
+        )}
       >
         {children}
       </body>
