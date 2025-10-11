b8980947bc68ddc9697c465f2e251c8ea44d51f8
Dalton Pettus <daltpettus@gmail.com>
2025-10-11 10:50:21 -0700

    build out types and basic structure of heading component

--- src/app/ui/Heading/Heading.tsx
+++ src/app/ui/Heading/Heading.tsx
@@ -1,5 +1,21 @@
+import { ComponentPropsWithoutRef, ReactNode } from "react";
+import styles from "./styles.module.css";
 
-export type HeadingLevel = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
-export default function Heading() {
+export type HeadingLevel = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+export type FontSize = "xs" | "sm" | "md" | "lg" | "xl" | "manual";
 
+export interface HeadingProps extends ComponentPropsWithoutRef<"h1"> {
+  as?: HeadingLevel;
+  size?: FontSize;
+  children: ReactNode;
+}
+
+export default function Heading({
+  as: Comp = "h2",
+  size = "lg",
+  children,
+  className,
+  ...restProps
+}: HeadingProps) {
+  return <Comp {...restProps}>{children}</Comp>;
 }
