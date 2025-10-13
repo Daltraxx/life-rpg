542602a3d177c6ed19f3c42464e175283605d07f
Dalton Pettus <daltpettus@gmail.com>
2025-10-13 12:02:10 -0700

    comment out unused styles import

--- src/app/ui/Heading/Heading.tsx
+++ src/app/ui/Heading/Heading.tsx
@@ -1,5 +1,5 @@
 import { ComponentPropsWithoutRef, ReactNode } from "react";
-import styles from "./styles.module.css";
+// import styles from "./styles.module.css";
 import clsx from "clsx";
 
 export type HeadingLevel = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
