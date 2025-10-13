b0b5e1da00425a84bb283a4862f5acdfb0bafc1d
Dalton Pettus <daltpettus@gmail.com>
2025-10-13 16:16:33 -0700

    use map for connecting selected fontsize to tailwind class

--- src/app/ui/Heading/Heading.tsx
+++ src/app/ui/Heading/Heading.tsx
@@ -1,4 +1,5 @@
 import { ComponentPropsWithoutRef, ReactNode } from "react";
+// import styles from "./styles.module.css";
 import clsx from "clsx";
 
 export type HeadingLevel = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
@@ -9,6 +10,14 @@ export interface HeadingProps extends ComponentPropsWithoutRef<"h1"> {
   size?: FontSize;
   children: ReactNode;
 }
+
+const fontSizeMap = {
+  "16": "text-base",
+  "36": "text-4xl",
+  "48": "text-5xl",
+  "72": "text-7xl",
+  "96": "text-8xl",
+};
 
 export default function Heading({
   as: Comp = "h2",
@@ -21,13 +30,7 @@ export default function Heading({
     <Comp
       className={clsx(
         "font-main",
-        size !== "manual" && {
-          "text-base": size === "16",
-          "text-4xl": size === "36",
-          "text-5xl": size === "48",
-          "text-7xl": size === "72",
-          "text-8xl": size === "96",
-        },
+        size !== "manual" && fontSizeMap[size],
         className
       )}
       {...restProps}
