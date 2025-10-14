812dff77063eb4b90e4ecf8a574630f08aa19863
Dalton Pettus <daltpettus@gmail.com>
2025-10-14 10:54:59 -0700

    remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Bounded from "./ui/Bounded";
-import Heading from "./ui/Heading";
 import LandingPageIntro from "./ui/LandingPage/LandingPageIntro/LandingPageIntro";
 
 export default function LandingPage() {
