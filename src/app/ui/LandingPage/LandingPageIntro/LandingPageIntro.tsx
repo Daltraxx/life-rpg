2e69082e57182c71d3f84518208fa72085ddf960
Dalton Pettus <daltpettus@gmail.com>
2025-10-14 11:04:49 -0700

    create css module for landingPageIntro and apply orange to main heading

--- src/app/ui/LandingPage/LandingPageIntro/LandingPageIntro.tsx
+++ src/app/ui/LandingPage/LandingPageIntro/LandingPageIntro.tsx
@@ -1,10 +1,11 @@
 import Bounded from "../../Bounded";
 import Heading from "../../Heading";
+import styles from "./styles.module.css";
 
 export default function LandingPageIntro() { 
   return (
     <Bounded>
-      <Heading as="h1" size="96">
+      <Heading as="h1" size="96" className={styles.mainHeading}>
         Life RPG
       </Heading>
     </Bounded>
