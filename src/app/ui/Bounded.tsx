1f56691f5acbf64ddea6caa415f24d2c99e0f7ef
Dalton Pettus <dpettus0713@gmail.com>
2025-10-13 17:48:53 -0700

    create Bounded component

--- src/app/ui/Bounded.tsx
+++ src/app/ui/Bounded.tsx
@@ -1,7 +1,19 @@
-
+import clsx from "clsx";
 
 type BoundedProps = {
-    as?: React.ElementType;
-    className?: string;
-    children: React.ReactNode;
+  as?: React.ElementType;
+  className?: string;
+  children: React.ReactNode;
+};
+
+export default function Bounded({
+  as: Comp = "section",
+  className,
+  children,
+}: BoundedProps) {
+  return (
+    <Comp className={clsx("px-4 py-8", className)}>
+      <div className="mx-auto w-full max-w-7xl">{children}</div>
+    </Comp>
+  );
 }
