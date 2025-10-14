7e639899e84ceada61cf4ca4d8b1f67a16380e6c
Dalton Pettus <daltpettus@gmail.com>
2025-10-14 11:35:51 -0700

    create html content of LandingPageIntro

--- src/app/ui/LandingPage/LandingPageIntro/LandingPageIntro.tsx
+++ src/app/ui/LandingPage/LandingPageIntro/LandingPageIntro.tsx
@@ -1,5 +1,6 @@
 import Bounded from "../../Bounded";
 import Heading from "../../Heading";
+import Text from "../../Text";
 import styles from "./styles.module.css";
 
 export default function LandingPageIntro() { 
@@ -8,6 +9,14 @@ export default function LandingPageIntro() {
       <Heading as="h1" size="96" className={styles.mainHeading}>
         Life RPG
       </Heading>
+      <Text size="24" className={styles.introParagraph}>
+        Grind your daily tasks, habits, and goals like one would their most
+        addictive RPG. Gain experience, level up, and get that dopamine via
+        on-screen progress that will be reflected in your real life mind,
+        discipline, and capabilities. Its a feedback loop that can be
+        mesmerizing in a video game. Now it can propel you to becoming
+        overpowered in your own dreams, in your own life.
+      </Text>
     </Bounded>
-  )
+  );
 }
