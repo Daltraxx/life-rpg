d51c2024f4c73f6a20c83e2f46420d51977d2945
Dalton Pettus <dpettus0713@gmail.com>
2025-10-13 18:01:19 -0700

    remove commented out import

--- src/app/ui/Heading.tsx
+++ src/app/ui/Heading.tsx
@@ -1,5 +1,4 @@
 import { ComponentPropsWithoutRef, ReactNode } from "react";
-// import styles from "./styles.module.css";
 import clsx from "clsx";
 
 export type HeadingLevel = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
