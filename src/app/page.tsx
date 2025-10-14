c873e95e9a1d2028c52ab3459265de33765bb38c
Dalton Pettus <daltpettus@gmail.com>
2025-10-14 10:52:47 -0700

    create LandingPageIntro component file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import Bounded from "./ui/Bounded";
 import Heading from "./ui/Heading";
 
 export default function LandingPage() {
   return (
-    <Heading as="h1" size="96">
-      Life RPG
-    </Heading>
+    <Bounded>
+      <Heading as="h1" size="96">
+        Life RPG
+      </Heading>
+    </Bounded>
   );
 }
---
+++ src/app/ui/LandingPage/LandingPageIntro/LandingPageIntro.tsx
@@ -0,0 +1,12 @@
+import Bounded from "../../Bounded";
+import Heading from "../../Heading";
+
+export default function LandingPageIntro() { 
+  return (
+    <Bounded>
+      <Heading as="h1" size="96">
+        Life RPG
+      </Heading>
+    </Bounded>
+  )
+}
