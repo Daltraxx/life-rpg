09efb47d8e7381e2ea2a4057cac24b6934fab476
Dalton Pettus <daltpettus@gmail.com>
2025-10-15 18:50:27 -0700

    continue filling out login component

--- src/app/ui/LandingPage/Login/Login.tsx
+++ src/app/ui/LandingPage/Login/Login.tsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+import styles from ',./styles.module.css';
+
 export default function Login() {
-  return <p>Login</p>;
+  return (
+    <form action="">
+      <label htmlFor="email-field">Email:</label>
+      <input type="email" id="email-field" name="email" required />
+      <label htmlFor="password-field">Password:</label>
+      <input type="password" id="password-field" name="password" required />
+      <button type="submit">Login</button>
+      <Link href="/forgot-password">Forgot Password?</Link>
+    </form>
+  );
 }
