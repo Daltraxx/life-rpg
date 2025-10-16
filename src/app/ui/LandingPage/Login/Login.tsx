76000eb381c0513449362ccb1b83af772d99a252
Dalton Pettus <daltpettus@gmail.com>
2025-10-15 18:55:29 -0700

    add some beginning styles to login form

--- src/app/ui/LandingPage/Login/Login.tsx
+++ src/app/ui/LandingPage/Login/Login.tsx
@@ -1,15 +1,18 @@
 import Link from "next/link";
 import styles from './styles.module.css';
+import Bounded from "../../Bounded";
 
 export default function Login() {
   return (
-    <form className={styles.loginForm} action="">
-      <label htmlFor="email-field">Email:</label>
-      <input type="email" id="email-field" name="email" required />
-      <label htmlFor="password-field">Password:</label>
-      <input type="password" id="password-field" name="password" required />
-      <button type="submit">Login</button>
-      <Link href="/forgot-password">Forgot Password?</Link>
-    </form>
+    <Bounded innerClassName={styles.loginContainer}>
+      <form className={styles.loginForm} action="">
+        <label htmlFor="email-field">Email:</label>
+        <input type="email" id="email-field" name="email" required />
+        <label htmlFor="password-field">Password:</label>
+        <input type="password" id="password-field" name="password" required />
+        <button type="submit">Login</button>
+        <Link href="/forgot-password">Forgot Password?</Link>
+      </form>
+    </Bounded>
   );
 }
