7cd2013d38e86afe527c1d3af458cd0b3f1fbf25
Dalton Pettus <daltpettus@gmail.com>
2025-10-15 18:36:42 -0700

    implement login component in root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Intro from "./ui/LandingPage/Intro/Intro";
 import styles from "./styles.module.css";
+import Login from "./ui/LandingPage/Login/Login";
 
 export default function LandingPage() {
   return (
     <div className={styles.landingPageContainer}>
       <Intro />
+      <Login />
     </div>
   );
 }
