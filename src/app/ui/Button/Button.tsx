0d90024c4347da6fcc2fced6edce201ba2d2dd3d
Dalton Pettus <daltpettus@gmail.com>
2025-10-18 15:59:26 -0700

    add color prop for deciding color class to buttons

--- src/app/ui/Button/Button.tsx
+++ src/app/ui/Button/Button.tsx
@@ -4,30 +4,45 @@ import Link from "next/link";
 import styles from "./styles.module.css";
 import clsx from "clsx";
 
+type Color = "brown-600" | "blue-600";
+
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  color?: Color;
   children: ReactNode;
 }
 
 interface LinkWrapperProps extends LinkProps {
+  color?: Color;
   children: ReactNode;
   className?: string;
 }
 
-export function Button({ children, className, ...restProps }: ButtonProps) {
+const colorMap = new Map<string, string>([
+  ["brown-600", styles.brown600],
+  ["blue-600", styles.blue600],
+]);
+
+export function Button({
+  color = "brown-600",
+  children,
+  className,
+  ...restProps
+}: ButtonProps) {
   return (
-    <button className={clsx(className, styles.button)} {...restProps}>
+    <button className={clsx(className, styles.button, colorMap.get(color))} {...restProps}>
       {children}
     </button>
   );
 }
 
 export function LinkWrapper({
+  color = "brown-600",
   children,
   className,
   ...restProps
 }: LinkWrapperProps) {
   return (
-    <Link className={clsx(className, styles.button)} {...restProps}>
+    <Link className={clsx(className, styles.button, colorMap.get(color))} {...restProps}>
       {children}
     </Link>
   );
