c89dbcba9da4511d2b929907b3c127b3e5cfdd09
Dalton Pettus <daltpettus@gmail.com>
2025-10-18 20:22:58 -0700

    style footer

--- src/app/ui/LandingPage/Footer/Footer.tsx
+++ src/app/ui/LandingPage/Footer/Footer.tsx
@@ -1,10 +1,16 @@
 import { RegularLinkWrapper } from "../../ButtonLinkWrappers/ButtonLinkWrappers";
 import Bounded from "../../Bounded";
+import styles from "./styles.module.css";
 
 export default function Footer() {
   return (
-    <Bounded as="footer">
-      <RegularLinkWrapper href="https://www.daltonpettus.com/" target="_blank" fontSize="36" color="orange-300">
+    <Bounded as="footer" innerClassName={styles.footer} verticalPadding={false}>
+      <RegularLinkWrapper
+        href="https://www.daltonpettus.com/"
+        target="_blank"
+        fontSize="36"
+        color="orange-300"
+      >
         by daltraxx INC.
       </RegularLinkWrapper>
     </Bounded>
