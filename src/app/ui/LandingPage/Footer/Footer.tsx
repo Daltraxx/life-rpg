4958094477807ddf5ca51706d142bbe4fbf524bb
Dalton Pettus <daltpettus@gmail.com>
2025-10-18 20:15:27 -0700

    add orange color to footer link

--- src/app/ui/LandingPage/Footer/Footer.tsx
+++ src/app/ui/LandingPage/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import Bounded from "../../Bounded";
 export default function Footer() {
   return (
     <Bounded as="footer">
-      <RegularLinkWrapper href="https://www.daltonpettus.com/" target="_blank" fontSize="36" >
+      <RegularLinkWrapper href="https://www.daltonpettus.com/" target="_blank" fontSize="36" color="orange-300">
         by daltraxx INC.
       </RegularLinkWrapper>
     </Bounded>
