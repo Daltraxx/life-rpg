4a82daf12b638a087d130e7606f5b1a75cfb415a
Dalton Pettus <daltpettus@gmail.com>
2025-10-18 20:45:32 -0700

    add aria label and rel to footer link

--- src/app/ui/LandingPage/Footer/Footer.tsx
+++ src/app/ui/LandingPage/Footer/Footer.tsx
@@ -8,6 +8,8 @@ export default function Footer() {
       <RegularLinkWrapper
         href="https://www.daltonpettus.com/"
         target="_blank"
+        aria-label="Visit creator's website at daltonpettus.com (opens in a new tab)"
+        rel="noopener noreferrer"
         fontSize="36"
         color="orange-300"
       >
