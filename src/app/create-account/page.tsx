332d708c88e06025bcbc5be076b04f7e0511f2a3
Dalton Pettus <daltpettus@gmail.com>
2025-10-20 18:57:56 -0700

    add starter code to create account/page.tsx

---
+++ src/app/create-account/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Create Account",
+};
+
+export default function CreateAccountPage() {
+  return (
+    <main>
+      <h1>Create Account</h1>
+      {/* Add your create account form or content here */}
+    </main>
+  );
+}
