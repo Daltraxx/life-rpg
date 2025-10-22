270d0cb5a9ad851f82e073209098548b1dff4a1d
Dalton Pettus <daltpettus@gmail.com>
2025-10-21 18:44:44 -0700

    style header

--- src/app/ui/account-creation/Header/Header.tsx
+++ src/app/ui/account-creation/Header/Header.tsx
@@ -1,10 +1,11 @@
 import Bounded from "../../Bounded";
 import Heading from "../../Heading";
+import styles from "./styles.module.css";
 
 export default function Header() {
   return (
-    <Bounded as="header">
-      <Heading as="h2" size="72">
+    <Bounded as="header" outerClassName={styles.boundedContainer} innerClassName={styles.headerContainer}>
+      <Heading as="h2" size="72" className={styles.branding}>
         LifeRPG
       </Heading>
     </Bounded>
