f5bbbd872e72e573482a587e94f5cdce6341c7ce
Dalton Pettus <daltpettus@gmail.com>
2025-10-21 19:03:14 -0700

    add branding and copyright with styles to footer

---
+++ src/app/ui/account-creation/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import Bounded from "../../Bounded";
+import Heading from "../../Heading";
+import styles from "./styles.module.css";
+import Text from '../../Text';
+import clsx from "clsx";
+
+export default function Footer() {
+  return (
+    <Bounded as="footer" outerClassName={styles.boundedContainer} innerClassName={styles.contentContainer} >
+      <Heading as="h4" size="72" className={styles.branding}>
+        LifeRPG
+      </Heading>
+      <Text as="span" size="20" className={clsx(styles.branding, styles.copyright)}>© {new Date().getFullYear()}</Text>
+    </Bounded>
+  );
+}
