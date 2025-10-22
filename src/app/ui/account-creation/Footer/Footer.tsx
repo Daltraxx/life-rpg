adbe6d93477096d6096b03dda73b602095fadfbe
Dalton Pettus <daltpettus@gmail.com>
2025-10-21 19:13:41 -0700

    add footer content with some styles

--- src/app/ui/account-creation/Footer/Footer.tsx
+++ src/app/ui/account-creation/Footer/Footer.tsx
@@ -1,16 +1,37 @@
 import Bounded from "../../Bounded";
 import Heading from "../../Heading";
 import styles from "./styles.module.css";
-import Text from '../../Text';
+import Text from "../../Text";
 import clsx from "clsx";
+import { RegularLinkWrapper } from "../../ButtonLinkWrappers/ButtonLinkWrappers";
 
 export default function Footer() {
   return (
-    <Bounded as="footer" outerClassName={styles.boundedContainer} innerClassName={styles.contentContainer} >
+    <Bounded
+      as="footer"
+      outerClassName={styles.boundedContainer}
+      innerClassName={styles.contentContainer}
+    >
       <Heading as="h4" size="72" className={styles.branding}>
         LifeRPG
       </Heading>
-      <Text as="span" size="20" className={clsx(styles.branding, styles.copyright)}>© {new Date().getFullYear()}</Text>
+      <Text
+        as="span"
+        size="20"
+        className={clsx(styles.branding, styles.copyright)}
+      >
+        © {new Date().getFullYear()}
+      </Text>
+      <RegularLinkWrapper
+        href="https://www.daltonpettus.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+        fontSize="30"
+        color="manual"
+        className={styles.daltraxxLink}
+      >
+        by Daltraxx INC.
+      </RegularLinkWrapper>
     </Bounded>
   );
 }
