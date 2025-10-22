11b3adf16e3bcfc561c22cdb852f9a3c30ba444f
Dalton Pettus <daltpettus@gmail.com>
2025-10-21 18:57:14 -0700

    rename classes

--- src/app/ui/account-creation/Header/Header.tsx
+++ src/app/ui/account-creation/Header/Header.tsx
@@ -4,7 +4,7 @@ import styles from "./styles.module.css";
 
 export default function Header() {
   return (
-    <Bounded as="header" outerClassName={styles.boundedContainer} innerClassName={styles.headerContainer}>
+    <Bounded as="header" outerClassName={styles.boundedContainer} innerClassName={styles.contentContainer}>
       <Heading as="h2" size="72" className={styles.branding}>
         LifeRPG
       </Heading>
