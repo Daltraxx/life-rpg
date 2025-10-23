91a41a8dbee06df2fc9fe1f9e0a76ff2359ac0f9
Dalton Pettus <daltpettus@gmail.com>
2025-10-22 17:13:54 -0700

    add intro text

--- src/app/ui/account-creation/CreateAccountForm/CreateAccountForm.tsx
+++ src/app/ui/account-creation/CreateAccountForm/CreateAccountForm.tsx
@@ -1,9 +1,20 @@
 import Bounded from "../../Bounded";
 import { ButtonWrapper } from "../../ButtonLinkWrappers/ButtonLinkWrappers";
+import Heading from "../../Heading";
+import Text from "../../Text";
 
 export default function CreateAccountForm() {
   return (
     <Bounded>
+      <div>
+        <Heading as="h1" size="48">
+          Welcome [new user]!
+        </Heading>
+        <Text as="p" size="36">
+          Please complete the following to set up your character profile...
+        </Text>
+      </div>
+
       <form>
         <label htmlFor="email">Email:</label>
         <input id="email" type="email" name="email" required />
