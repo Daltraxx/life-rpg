ae75031b7f39cfb6fe0de814962f1db47b67faa9
Dalton Pettus <daltpettus@gmail.com>
2025-10-23 17:00:17 -0700

    make account creation page take full viewport height

--- src/app/(account-creation)/layout.tsx
+++ src/app/(account-creation)/layout.tsx
@@ -1,11 +1,12 @@
 import Header from "@/app/ui/account-creation/Header/Header";
 import Footer from "@/app/ui/account-creation/Footer/Footer";
+import styles from "./styles.module.css";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <Header />
-      <main>{children}</main>
+      <main className={styles.mainContent}>{children}</main>
       <Footer />
     </div>
   );
