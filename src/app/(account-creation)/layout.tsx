40be228255206a2d1042a9c27059d7a2377d1b0a
Dalton Pettus <daltpettus@gmail.com>
2025-10-24 18:35:34 -0700

    use ReactNode directly

--- src/app/(account-creation)/layout.tsx
+++ src/app/(account-creation)/layout.tsx
@@ -1,8 +1,9 @@
+import { ReactNode } from "react";
 import Header from "@/app/ui/account-creation/Header/Header";
 import Footer from "@/app/ui/account-creation/Footer/Footer";
 import styles from "./styles.module.css";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
       <Header />
