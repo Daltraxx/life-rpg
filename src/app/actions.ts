4d0183d69ce3bfcb2221181ae6d8b0cc8ef2b608
Dalton Pettus <daltpettus@gmail.com>
2025-11-04 17:22:52 -0800

    add starting code for login server action

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,6 +1,51 @@
 'use server';
 
+import { z } from "zod";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 import { createSupabaseServerClient } from "@/utils/supabase/server";
+
+// Further validation can be added here as needed
+const LoginSchema = z.object({
+  email: z.email(),
+  password: z.string().min(6).max(100),
+});
+
+export type LoginState = {
+  errors?: {
+    email?: string[];
+    password?: string[];
+  };
+  message?: string | null;
+};
+
+export async function login(prevState: LoginState, formData: FormData) {
+  const supabase = await createSupabaseServerClient();
+
+  const rawFormData = {
+    email: formData.get("email"),
+    password: formData.get("password"),
+  };
+
+  const validatedFields = LoginSchema.safeParse(rawFormData);
+
+  if (!validatedFields.success) {
+    return {
+      errors: z.treeifyError(validatedFields.error), // test this
+      message: "Fields not valid. Failed to log in.",
+    } as LoginState;
+  }
+
+  const { error } = await supabase.auth.signInWithPassword(validatedFields.data);
+
+  if (error) {
+    return {
+      message: error.message, // test this
+    } as LoginState;
+  }
+
+  // Revalidate the path to update any server components depending on auth state
+  revalidatePath("/");
+  redirect("/profile"); // Redirect to profile or desired page after login (need to create page)
+}
