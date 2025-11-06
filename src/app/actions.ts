770947fd6ce7c95457ef5265f18354508b7d587c
Dalton Pettus <daltpettus@gmail.com>
2025-11-06 11:01:30 -0800

    increase min password length

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -9,7 +9,7 @@ import { createSupabaseServerClient } from "@/utils/supabase/server";
 // Further validation can be added here as needed
 const LoginSchema = z.object({
   email: z.email(),
-  password: z.string().min(6).max(100),
+  password: z.string().min(8).max(100),
 });
 
 export type LoginState = {
