ccab9fabb007e096cc7c23cc9fe82288703c022c
Dalton Pettus <daltpettus@gmail.com>
2025-11-06 11:38:52 -0800

    add createAccount server action

--- src/app/(account-creation)/actions.ts
+++ src/app/(account-creation)/actions.ts
@@ -13,3 +13,52 @@ const SignupSchema = z.object({
   password: z.string().min(8).max(100),
   confirmPassword: z.string().min(8).max(100),
 });
+
+export type SignupState = {
+  errors?: {
+    email?: string[];
+    username?: string[];
+    password?: string[];
+    confirmPassword?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createAccount(prevState: SignupState, formData: FormData) {
+  const supabase = await createSupabaseServerClient();
+
+  const rawFormData = {
+    email: formData.get("email"),
+    username: formData.get("username"),
+    password: formData.get("password"),
+    confirmPassword: formData.get("confirmPassword"),
+  };
+
+  const validatedFields = SignupSchema.safeParse(rawFormData);
+
+  if (!validatedFields.success) {
+    return {
+      errors: z.flattenError(validatedFields.error), // test this
+      message: "Fields not valid. Failed to create account.",
+    } as SignupState;
+  }
+
+  const { error } = await supabase.auth.signUp({
+    email: validatedFields.data.email,
+    password: validatedFields.data.password,
+  });
+
+  if (error) {
+    return {
+      message: "Account creation failed. Please try again.",
+    } as SignupState;
+  }
+  
+  // Revalidate the path to update any server components depending on auth state
+  // Consider more targeted path revalidation.
+  // Revalidating the root path "/" may be broader than necessary.
+  // If only specific pages depend on auth state,
+  // consider revalidating those paths specifically(e.g., "/profile", "/dashboard").
+  revalidatePath("/");
+  redirect("/profile"); // Redirect to profile or desired page after login (need to create page)
+}
