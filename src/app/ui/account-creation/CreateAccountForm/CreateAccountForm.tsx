9134b77ea203e44f7d279084d221ff8dddbd1ae6
Dalton Pettus <daltpettus@gmail.com>
2025-11-07 11:24:09 -0800

    fix zod import syntax

--- src/app/ui/account-creation/CreateAccountForm/CreateAccountForm.tsx
+++ src/app/ui/account-creation/CreateAccountForm/CreateAccountForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import z from "zod";
+import { z } from "zod";
 import { useState, useEffect, useActionState } from "react";
 import { createAccount } from "@/app/(account-creation)/actions";
 import { SignupSchema, SignupState } from "@/utils/validations/signup";
