5936a4a76b75233d35ea728f0955346cc32a69eb
Dalton Pettus <daltpettus@gmail.com>
2025-11-07 17:36:45 -0800

    make branding logo clickable

--- src/app/ui/account-creation/Header/Header.tsx
+++ src/app/ui/account-creation/Header/Header.tsx
@@ -1,13 +1,20 @@
+import Link from "next/link";
 import Bounded from "../../Bounded";
 import Heading from "../../Heading";
 import styles from "./styles.module.css";
 
 export default function Header() {
   return (
-    <Bounded as="header" outerClassName={styles.boundedContainer} innerClassName={styles.contentContainer}>
-      <Heading as="h2" size="72" className={styles.branding}>
-        LifeRPG
-      </Heading>
+    <Bounded
+      as="header"
+      outerClassName={styles.boundedContainer}
+      innerClassName={styles.contentContainer}
+    >
+      <Link href="/" className={styles.brandingLink}>
+        <Heading as="h2" size="72" className={styles.branding}>
+          LifeRPG
+        </Heading>
+      </Link>
     </Bounded>
   );
 }
