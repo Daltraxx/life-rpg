af56e407f8107aa359a24ef030230eb88e1cc721
Dalton Pettus <daltpettus@gmail.com>
2025-11-08 14:57:48 -0800

    extend ComponentProps instead of ComponentPropsWithoutRef and remove manual ref prop

--- src/app/ui/Heading.tsx
+++ src/app/ui/Heading.tsx
@@ -1,13 +1,12 @@
-import { ComponentPropsWithoutRef, ReactNode } from "react";
+import { ComponentProps, ReactNode } from "react";
 import clsx from "clsx";
 import { fontSizeToTWMap, FontSize } from "./utils/fontSizeToTWMap";
 
 type HeadingLevel = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 
-export interface HeadingProps extends ComponentPropsWithoutRef<"h1"> {
+export interface HeadingProps extends ComponentProps<"h1"> {
   as?: HeadingLevel;
   size?: FontSize;
-  ref?: React.Ref<HTMLHeadingElement>;
   children: ReactNode;
 }
 
