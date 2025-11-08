213a0439abcd1531f0b84a99cf9730f4a16eab5d
Dalton Pettus <daltpettus@gmail.com>
2025-11-07 19:19:13 -0800

    make so confirmPassword only has to match

--- src/utils/validations/signup.ts
+++ src/utils/validations/signup.ts
@@ -18,8 +18,6 @@ export const SignupSchema = z
       ),
     confirmPassword: z
       .string()
-      .min(8, "Password must be at least 8 characters long")
-      .max(100, "Password must be at most 100 characters long"),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords do not match",
