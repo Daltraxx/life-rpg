c33de9590a6aaf2ef9a673b6935ed240213af832
Dalton Pettus <daltpettus@gmail.com>
2025-11-14 11:36:40 -0800

    remove redundant min length check on password regex

--- src/utils/validations/signup.ts
+++ src/utils/validations/signup.ts
@@ -14,7 +14,7 @@ export const SignupSchema = z
       .min(8, "Password must be at least 8 characters long")
       .max(100, "Password must be at most 100 characters long")
       .regex(
-        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).*$/,
         "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
       ),
     confirmPassword: z.string(),
