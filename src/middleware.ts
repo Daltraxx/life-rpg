9684f15ac08ec7ded178d427f9c2b6e64c5f13d5
Dalton Pettus <daltpettus@gmail.com>
2025-11-14 11:46:46 -0800

    exclude some other common files in matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,8 +12,9 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - robots.txt, sitemap.xml, manifest.json (common static files)
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|manifest.json|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
