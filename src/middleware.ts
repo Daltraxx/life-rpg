3e93fb3c5f5c1bb2e892601dcca49e9b6010e950
Dalton Pettus <daltpettus@gmail.com>
2025-11-14 18:51:15 -0800

    add type=module to package.json to resolve middleware syntax error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse, type NextRequest } from "next/server";
 import { updateSession } from "@/utils/supabase/middleware";
 
-export async function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest): Promise<NextResponse> {
   try {
     return await updateSession(request);
   } catch (error) {
