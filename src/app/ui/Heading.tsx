ba7318f5800c316134c7583bb43792312a2f4588
Dalton Pettus <daltpettus@gmail.com>
2025-11-17 08:51:06 -0800

    document that forwarding refs is no longer necessary as of React 19

--- src/app/ui/Heading.tsx
+++ src/app/ui/Heading.tsx
@@ -4,6 +4,7 @@ import { fontSizeToTWMap, FontSize } from "./utils/fontSizeToTWMap";
 
 type HeadingLevel = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 
+// NOTE: Forwarding refs is no longer necessary in React 19
 export interface HeadingProps extends ComponentProps<"h1"> {
   as?: HeadingLevel;
   size?: FontSize;
