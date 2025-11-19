26dc7bad0bb5e07bca92f63701604746bdc53e86
Dalton Pettus <daltpettus@gmail.com>
2025-11-18 18:40:31 -0800

    preserve original error stack trace

--- src/app/queries/client/checkIfUsernameExists.ts
+++ src/app/queries/client/checkIfUsernameExists.ts
@@ -38,7 +38,8 @@ export default async function checkIfUsernameExists(
     throw new Error(
       `Error checking existing user: ${
         error instanceof Error ? error.message : String(error)
-      }`
+      }`,
+      { cause: error }
     );
   }
 }
