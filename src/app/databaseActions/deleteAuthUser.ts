73d70efbabe73070a9393e06f60d5397490c5477
Dalton Pettus <daltpettus@gmail.com>
2025-11-19 19:12:55 -0800

    add auth action for deleting user from supabase auth.users

--- src/app/databaseActions/deleteAuthUser.ts
+++ src/app/databaseActions/deleteAuthUser.ts
@@ -2,6 +2,16 @@
 
 import { createSupabaseServerClient } from "@/utils/supabase/server";
 
-export async function deleteAuthUser() {
-  const supabase = await createSupabaseServerClient();
+export async function deleteAuthUser(userId: string): Promise<void> {
+  const supabaseAdmin = await createSupabaseServerClient({ admin: true });
+  const { error } = await supabaseAdmin.auth.admin.deleteUser(userId);
+  if (error) {
+    console.error("Error deleting user:", error);
+    throw new Error("Failed to delete user account.");
+  }
+
+  // Delete user data from other tables if exists
+  await supabaseAdmin.from("users").delete().eq("id", userId);
+
+  await supabaseAdmin.auth.signOut();
 }
