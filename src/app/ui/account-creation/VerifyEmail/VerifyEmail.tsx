bbe7d7ddd039b6be9629195ec32aacbdf1a3af15
Dalton Pettus <daltpettus@gmail.com>
2025-11-25 08:38:25 -0800

    style verify email page

--- src/app/ui/account-creation/VerifyEmail/VerifyEmail.tsx
+++ src/app/ui/account-creation/VerifyEmail/VerifyEmail.tsx
@@ -1,12 +1,19 @@
+import Bounded from "../../Bounded";
+import Heading from "../../Heading";
+import { Paragraph } from "../../TextWrappers";
 import styles from "./styles.module.css";
 
 export default function VerifyEmail() {
   return (
-    <section role="status" aria-live="polite" className={styles.verifyEmail}>
-      <h1>Account Created Successfully!</h1>
-      <p>
-        Please check your email to verify your account so we can get started.
-      </p>
-    </section>
+    <Bounded innerClassName={styles.contentContainer}>
+      <section role="status" aria-live="polite" className={styles.verifyEmailSection}>
+        <Heading as="h1" size="48-responsive">
+          Account Created Successfully!
+        </Heading>
+        <Paragraph size="36-responsive">
+          Please check your email to verify your account so we can get started.
+        </Paragraph>
+      </section>
+    </Bounded>
   );
 }
