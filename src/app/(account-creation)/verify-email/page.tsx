2804bddbaabb5047cd632d880f17cc3ad530c48e
Dalton Pettus <daltpettus@gmail.com>
2025-11-24 19:02:52 -0800

    add md description

--- src/app/(account-creation)/verify-email/page.tsx
+++ src/app/(account-creation)/verify-email/page.tsx
@@ -3,6 +3,7 @@ import VerifyEmail from "@/app/ui/account-creation/VerifyEmail/VerifyEmail";
 import { type Metadata } from "next";
 export const metadata: Metadata = {
   title: "Verify Email",
+  description: "Verify your email to complete account creation.",
 };
 
 export default function VerifyEmailPage() {
