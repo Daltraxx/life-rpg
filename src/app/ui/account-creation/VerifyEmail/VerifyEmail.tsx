3d5825576def061b0cb06557e30491fba52b4663
Dalton Pettus <daltpettus@gmail.com>
2025-11-24 18:23:50 -0800

    create bones of verify-email page

---
+++ src/app/ui/account-creation/VerifyEmail/VerifyEmail.tsx
@@ -0,0 +1,11 @@
+
+
+
+export default function VerifyEmail() { 
+  return (
+    <section>
+      <h1>Account Created Successfully!</h1>
+      <p>Please check your provided email to verify your account, then we can get started.</p>
+    </section>
+  )
+}
