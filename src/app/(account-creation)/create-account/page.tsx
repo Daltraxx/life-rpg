fed9f79d094f7153e5a0115d73aa74417c77188f
Dalton Pettus <daltpettus@gmail.com>
2025-11-24 19:00:09 -0800

    metadata description update

--- src/app/(account-creation)/create-account/page.tsx
+++ src/app/(account-creation)/create-account/page.tsx
@@ -3,6 +3,8 @@ import CreateAccountForm from "@/app/ui/account-creation/CreateAccountForm/Creat
 import { type Metadata } from "next";
 export const metadata: Metadata = {
   title: "Create Account",
+  description:
+    "Improve the sense of feedback and reward you receive from accomplishing your daily goals. Create an account to get started with Life RPG.",
 };
 
 export default function CreateAccountPage() {
