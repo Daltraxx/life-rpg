0a9cba384a0fb1caccb2bf08e9198cb3486c6dd6
Dalton Pettus <daltpettus@gmail.com>
2025-11-24 18:54:22 -0800

    copy change

--- src/app/ui/account-creation/VerifyEmail/VerifyEmail.tsx
+++ src/app/ui/account-creation/VerifyEmail/VerifyEmail.tsx
@@ -5,7 +5,7 @@ export default function VerifyEmail() {
   return (
     <section>
       <h1>Account Created Successfully!</h1>
-      <p>Please check your provided email to verify your account, then we can get started.</p>
+      <p>Please check your email to verify your account so we can get started.</p>
     </section>
   )
 }
