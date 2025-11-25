16c67b813c288fef2c0499d8e2a2359698ab2e92
Dalton Pettus <daltpettus@gmail.com>
2025-11-24 18:56:07 -0800

    import styles

--- src/app/ui/account-creation/VerifyEmail/VerifyEmail.tsx
+++ src/app/ui/account-creation/VerifyEmail/VerifyEmail.tsx
@@ -1,11 +1,11 @@
-
-
-
-export default function VerifyEmail() { 
+import styles from "./VerifyEmail.module.css";
+export default function VerifyEmail() {
   return (
-    <section role="status" aria-live="polite">
+    <section role="status" aria-live="polite" className={styles.verifyEmail}>
       <h1>Account Created Successfully!</h1>
-      <p>Please check your email to verify your account so we can get started.</p>
+      <p>
+        Please check your email to verify your account so we can get started.
+      </p>
     </section>
-  )
+  );
 }
