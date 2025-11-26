0c1bcba3f074352cd06f627d2d32fe63b9476e8c
Dalton Pettus <daltpettus@gmail.com>
2025-11-26 12:15:25 -0800

    add todo

--- src/app/ui/account-creation/VerifyEmail/VerifyEmail.tsx
+++ src/app/ui/account-creation/VerifyEmail/VerifyEmail.tsx
@@ -4,6 +4,11 @@ import { Paragraph } from "@/app/ui/JSXWrappers/TextWrappers";
 import styles from "./styles.module.css";
 
 export default function VerifyEmail() {
+  // TODO:
+  // Display the email address where the verification was sent
+  // A "Resend verification email" button in case they don't receive it
+  // A link to navigate back to login or home page
+  // Additional guidance on what to do if the email doesn't arrive (e.g., check spam folder)
   return (
     <Bounded
       outerClassName={styles.outerContainer}
