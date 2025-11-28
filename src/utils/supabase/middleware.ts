d415a8a5d15c0b54bfeb222b34f3f2cfbd9a4e31
Dalton Pettus <daltpettus@gmail.com>
2025-11-27 19:14:14 -0800

    formatting

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -92,16 +92,11 @@ export async function updateSession(
     error,
   } = await supabase.auth.getUser();
 
-  const publicPaths = [
-    "/",
-    "/create-account",
-    "/auth",
-    "/error",
-  ];
+  const publicPaths = ["/", "/create-account", "/auth", "/error"];
   const cookies = request.cookies;
   if (cookies.get("unverified_signup") && user?.email_confirmed_at)
     // Remove unverified_signup cookie if user has verified their email
-     supabaseResponse.cookies.delete("unverified_signup");
+    supabaseResponse.cookies.delete("unverified_signup");
   if (cookies.get("unverified_signup"))
     // Allow access to verify-email page if unverified_signup cookie is present
     publicPaths.push("/verify-email");
