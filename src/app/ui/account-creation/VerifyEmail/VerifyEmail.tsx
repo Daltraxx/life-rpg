df76925f3cc71d64f539728effd50137d979e2f7
Dalton Pettus <daltpettus@gmail.com>
2025-11-30 14:02:21 -0800

    add fallback placeholder email if email is empty or null

--- src/app/ui/account-creation/VerifyEmail/VerifyEmail.tsx
+++ src/app/ui/account-creation/VerifyEmail/VerifyEmail.tsx
@@ -3,11 +3,15 @@ import Heading from "@/app/ui/JSXWrappers/Heading";
 import { Paragraph } from "@/app/ui/JSXWrappers/TextWrappers";
 import styles from "./styles.module.css";
 
+const PLACEHOLDER_EMAIL = "your email";
 export default function VerifyEmail({ email }: { email: string }) {
   // TODO:
   // A "Resend verification email" button in case they don't receive it
   // A link to navigate back to login or home page
   // Additional guidance on what to do if the email doesn't arrive (e.g., check spam folder)
+  
+  email = email?.trim() || PLACEHOLDER_EMAIL;
+
   return (
     <Bounded
       outerClassName={styles.outerContainer}
@@ -18,7 +22,8 @@ export default function VerifyEmail({ email }: { email: string }) {
           Account Created Successfully!
         </Heading>
         <Paragraph size="36-responsive">
-          Please check <strong>{email}</strong> to verify your account so we can get started.
+          Please check <strong>{email}</strong> to verify your account so we can
+          get started.
         </Paragraph>
       </section>
     </Bounded>
