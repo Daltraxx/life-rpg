9e565d1df8f903ba385a3a8c185a08d0d833c42e
Dalton Pettus <daltpettus@gmail.com>
2025-12-01 14:16:03 -0800

    group imports

--- src/app/(account-creation)/account-setup/page.tsx
+++ src/app/(account-creation)/account-setup/page.tsx
@@ -1,6 +1,6 @@
 import Intro from "@/app/ui/account-creation/AccountSetup/Intro/Intro";
-
 import type { Metadata } from "next";
+
 export const metadata: Metadata = {
   title: "Account Setup",
   description: `Set up your account to start progressing with Life RPG.
