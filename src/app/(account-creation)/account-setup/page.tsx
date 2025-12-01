dc9980fd81c6f0547af281210f3016e52855dcb7
Dalton Pettus <daltpettus@gmail.com>
2025-12-01 15:03:12 -0800

    fix metadata description

--- src/app/(account-creation)/account-setup/page.tsx
+++ src/app/(account-creation)/account-setup/page.tsx
@@ -3,10 +3,9 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Account Setup",
-  description: `Set up your account to start progressing with Life RPG.
-    Choose the attributes you wish to level, and the daily quests that will get you there.`,
+  description:
+    "Set up your account to start progressing with Life RPG. Choose the attributes you wish to level, and the daily quests that will get you there.",
 };
-
 export default function AccountSetupPage() {
   return <Intro />;
 }
