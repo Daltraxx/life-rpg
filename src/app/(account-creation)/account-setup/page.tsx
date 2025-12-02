aae84b1991c0d664a371d42043d47864a63d8603
Dalton Pettus <daltpettus@gmail.com>
2025-12-01 17:10:08 -0800

    pass authenticated user to Intro component

--- src/app/(account-creation)/account-setup/page.tsx
+++ src/app/(account-creation)/account-setup/page.tsx
@@ -1,11 +1,16 @@
 import Intro from "@/app/ui/account-creation/AccountSetup/Intro/Intro";
 import type { Metadata } from "next";
+import { createSupabaseServerClient } from "@/utils/supabase/server";
 
 export const metadata: Metadata = {
   title: "Account Setup",
   description:
     "Set up your account to start progressing with Life RPG. Choose the attributes you wish to level, and the daily quests that will get you there.",
 };
-export default function AccountSetupPage() {
-  return <Intro />;
+export default async function AccountSetupPage() {
+  const supabase = await createSupabaseServerClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  return <Intro authUser={user} />;
 }
