d657a3e81383ed6bcd6a6b6adcb21fc9ae2310b4
Dalton Pettus <daltpettus@gmail.com>
2025-12-05 15:07:18 -0800

    add return type

--- src/app/ui/account-creation/account-setup/SetupUI/SetupUI.tsx
+++ src/app/ui/account-creation/account-setup/SetupUI/SetupUI.tsx
@@ -1,7 +1,8 @@
 import Bounded from "@/app/ui/JSXWrappers/Bounded";
 import AttributeWidget from "./AttributeWidget/AttributeWidget";
+import { JSX } from "react";
 
-export default function SetupUI() {
+export default function SetupUI(): JSX.Element {
   return (
     <Bounded>
       <AttributeWidget />
