a6d920883c8066940ed6897ea99e4af2fc272d9e
Dalton Pettus <daltpettus@gmail.com>
2025-12-04 16:20:21 -0800

    add documentation

--- src/app/ui/account-creation/AccountSetup/AttributeWidget/AttributeList/AttributeListItem.tsx
+++ src/app/ui/account-creation/AccountSetup/AttributeWidget/AttributeList/AttributeListItem.tsx
@@ -3,16 +3,37 @@ import { faRectangleXmark } from "@fortawesome/free-regular-svg-icons";
 import clsx from "clsx";
 import { ListItem } from "@/app/ui/JSXWrappers/TextWrappers";
 import styles from "./styles.module.css";
+import { JSX } from "react";
 
 interface AttributeListItemProps {
   attribute: string;
   onDelete: (attribute: string) => void;
 }
 
+/**
+ * Renders a single attribute list item with a remove button.
+ * 
+ * @component
+ * @param {AttributeListItemProps} props - The component props
+ * @param {string} props.attribute - The name of the attribute to display
+ * @param {(attribute: string) => void} props.onDelete - Callback function invoked when the remove button is clicked
+ * @returns {JSX.Element} A list item containing the attribute name and a remove button. The Discipline attribute cannot be removed and is visually indicated as required.
+ * 
+ * @example
+ * <AttributeListItem 
+ *   attribute="Strength" 
+ *   onDelete={(attr) => console.log(`Removed ${attr}`)} 
+ * />
+ * 
+ * @remarks
+ * - The "Discipline" attribute is a required attribute and cannot be removed
+ * - The remove button styling differs for the Discipline attribute to indicate its required status
+ * - Uses FontAwesome XMark icon for the remove button
+ */
 export default function AttributeListItem({
   attribute,
   onDelete,
-}: AttributeListItemProps) {
+}: AttributeListItemProps): JSX.Element {
   const isDiscipline = attribute === "Discipline";
 
   return (
