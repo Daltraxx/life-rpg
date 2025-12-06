af617720d3cfe3708c82952586125223ceac3400
Dalton Pettus <daltpettus@gmail.com>
2025-12-06 14:43:55 -0800

    display QuestsWidget in SetupUI

--- src/app/ui/account-creation/account-setup/SetupUI/SetupUI.tsx
+++ src/app/ui/account-creation/account-setup/SetupUI/SetupUI.tsx
@@ -1,10 +1,12 @@
 import Bounded from "@/app/ui/JSXWrappers/Bounded";
 import AttributeWidget from "./AttributeWidget/AttributeWidget";
+import QuestsWidget from "./QuestsWidget/QuestsWidget";
 
 export default function SetupUI() {
   return (
     <Bounded>
       <AttributeWidget />
+      <QuestsWidget />
     </Bounded>
   );
 }
