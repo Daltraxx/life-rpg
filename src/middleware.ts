3ff50852d08298c329aedd4c46ad6aa793a0eaf4
Dalton Pettus <daltpettus@gmail.com>
2025-12-06 11:06:56 -0800

    match only root error page in matcher config

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,6 +26,6 @@ export const config = {
      * - any files with extensions commonly used for images and icons
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|manifest.json|error|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|manifest.json|error(?:/|$)|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
