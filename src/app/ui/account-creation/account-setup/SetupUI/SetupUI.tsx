7ed5575dfd183652a76f05cc7be9e830b7e479e8
Dalton Pettus <daltpettus@gmail.com>
2025-12-13 12:53:42 -0800

    extrad grid vars to Component scoped file

--- src/app/ui/account-creation/account-setup/SetupUI/SetupUI.tsx
+++ src/app/ui/account-creation/account-setup/SetupUI/SetupUI.tsx
@@ -2,13 +2,15 @@ import Bounded from "@/app/ui/JSXWrappers/Bounded";
 import AttributeWidget from "./AttributeWidget/AttributeWidget";
 import QuestsWidget from "./QuestsWidget/QuestsWidget";
 import styles from "./styles.module.css";
+import cssVars from "@/app/ui/account-creation/account-setup/SetupUI/QuestsWidget/vars.module.css"
+import clsx from "clsx";
 
 export default function SetupUI() {
   return (
     <Bounded>
       <div className={styles.uiContainer}>
         <AttributeWidget className={styles.attributeWidget} />
-        <QuestsWidget className={styles.questsWidget} />
+        <QuestsWidget className={clsx(styles.questsWidget, cssVars.gridVars)} />
       </div>
     </Bounded>
   );
