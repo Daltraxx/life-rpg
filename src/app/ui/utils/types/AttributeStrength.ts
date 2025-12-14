bd3d5516542d943f687e2e47398c4cb73409144b
Dalton Pettus <daltpettus@gmail.com>
2025-12-14 13:17:46 -0800

    fix doc

--- src/app/ui/utils/types/AttributeStrength.ts
+++ src/app/ui/utils/types/AttributeStrength.ts
@@ -1,8 +1,4 @@
-
 /**
  * Represents internal representation of strength level of an attribute.
- * @typedef {("normal" | "plus" | "plusPlus")} AttributeStrength
- * @example
- * const strength: AttributeStrength = "plus";
  */
 export type AttributeStrength = "normal" | "plus" | "plusPlus";
