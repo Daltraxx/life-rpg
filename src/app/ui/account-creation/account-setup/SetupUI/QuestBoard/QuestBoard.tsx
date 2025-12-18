eae8c3faf1b8f4998b190be0443d34c94a7bf035
Dalton Pettus <daltpettus@gmail.com>
2025-12-18 15:03:36 -0800

    start adding questboard container and add background color

--- src/app/ui/account-creation/account-setup/SetupUI/QuestBoard/QuestBoard.tsx
+++ src/app/ui/account-creation/account-setup/SetupUI/QuestBoard/QuestBoard.tsx
@@ -7,6 +7,9 @@ export default function QuestBoard() {
       <Heading as="h2" color="blue-700" size="36">
         Current Quest Board
       </Heading>
+      <section className={styles.questBoard}>
+        <Heading as="h3" color="background" size="36">Quests for today:</Heading>
+      </section>
     </section>
   );
 }
