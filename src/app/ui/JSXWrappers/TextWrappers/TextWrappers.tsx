35b3272046f5a94219dc57c1e78cd89cf4c42c39
Dalton Pettus <daltpettus@gmail.com>
2025-12-18 18:30:02 -0800

    update docs

--- src/app/ui/JSXWrappers/TextWrappers/TextWrappers.tsx
+++ src/app/ui/JSXWrappers/TextWrappers/TextWrappers.tsx
@@ -26,7 +26,7 @@ interface TextWrapperProps {
  *
  * @template T - The HTML element type to wrap ("p", "span", "label", or "li").
  * @param element - The HTML element tag to use for the wrapper.
- * The returned component accepts a `size` prop to control font size, a `className` prop for custom classes,
+ * The returned component accepts a `size` prop to control font size, a `color` prop for text color, a `className` prop for custom classes,
  * and any other valid props for the specified element type. The `children` prop is used to render content inside the element.
  *
  * @example
