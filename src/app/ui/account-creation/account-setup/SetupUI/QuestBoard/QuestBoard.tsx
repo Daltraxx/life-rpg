f9def54e20d6d1fbbd5f96492704bbb14691a34f
Dalton Pettus <daltpettus@gmail.com>
2025-12-18 17:35:11 -0800

    add starter code for QuestBoardItems

--- src/app/ui/account-creation/account-setup/SetupUI/QuestBoard/QuestBoard.tsx
+++ src/app/ui/account-creation/account-setup/SetupUI/QuestBoard/QuestBoard.tsx
@@ -1,6 +1,7 @@
 import { Quest } from "@/app/ui/utils/classesAndInterfaces/AttributesAndQuests";
 import styles from "./styles.module.css";
 import Heading from "@/app/ui/JSXWrappers/Heading/Heading";
+import QuestBoardItems from "./QuestBoardItems/QuestItems";
 
 export interface QuestBoardProps {
   quests: Quest[];
@@ -15,7 +16,7 @@ export default function QuestBoard({ quests }: QuestBoardProps) {
       <section className={styles.questBoard}>
         <Heading as="h3" color="background" size="36">Quests for today:</Heading>
         <div>
-          
+          <QuestBoardItems quests={quests} />
         </div>
       </section>
     </section>
---
+++ src/app/ui/account-creation/account-setup/SetupUI/QuestBoard/QuestBoardItems/QuestItems.tsx
@@ -0,0 +1,9 @@
+import { Quest } from "@/app/ui/utils/classesAndInterfaces/AttributesAndQuests";
+
+interface QuestBoardItemsProps {
+  quests: Quest[];
+}
+
+export default function QuestBoardItems({ quests }: QuestBoardItemsProps) {
+  return <></>;
+}
