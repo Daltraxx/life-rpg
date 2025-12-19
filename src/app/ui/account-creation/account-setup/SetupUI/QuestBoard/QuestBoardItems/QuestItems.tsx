b2a932a1aae048466d45b0dfe8d50bab7c0f01c0
Dalton Pettus <daltpettus@gmail.com>
2025-12-18 18:07:46 -0800

    add basic styling

--- src/app/ui/account-creation/account-setup/SetupUI/QuestBoard/QuestBoardItems/QuestItems.tsx
+++ src/app/ui/account-creation/account-setup/SetupUI/QuestBoard/QuestBoardItems/QuestItems.tsx
@@ -1,3 +1,7 @@
+import { ButtonWrapper } from "@/app/ui/JSXWrappers/ButtonLikeWrappers/ButtonLikeWrappers";
+import styles from "./styles.module.css";
+import Heading from "@/app/ui/JSXWrappers/Heading/Heading";
+import { Paragraph } from "@/app/ui/JSXWrappers/TextWrappers/TextWrappers";
 import { Quest } from "@/app/ui/utils/classesAndInterfaces/AttributesAndQuests";
 
 interface QuestBoardItemsProps {
@@ -5,5 +9,27 @@ interface QuestBoardItemsProps {
 }
 
 export default function QuestBoardItems({ quests }: QuestBoardItemsProps) {
-  return <></>;
+  return (
+    <>
+      {quests.map((quest) => (
+        <div key={quest.order}>
+          <Heading as="h4" color="background">{quest.name}</Heading>
+          {/* Attributes */}
+          <Paragraph size="16" color="background">
+            Streak: 0
+          </Paragraph>
+          <Paragraph size="16" color="background">
+            Strength: 0 — E
+          </Paragraph>
+          <div className={styles.experienceGainedSection}>
+            <Paragraph size="16" color="background">Experience Gained: {0}</Paragraph>
+            <div>{/* Exp toggle buttons */}</div>
+          </div>
+          <ButtonWrapper className={styles.completeQuestButton} color="background">
+            DELETE QUEST
+          </ButtonWrapper>
+        </div>
+      ))}
+    </>
+  );
 }
