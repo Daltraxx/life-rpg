a7488f11d734ff6a2cc98030fec8ce0b3c857f4c
Dalton Pettus <daltpettus@gmail.com>
2025-12-19 18:02:46 -0800

    add chevron-up svg icon

---
+++ src/app/ui/Icons/ChevronUp.tsx
@@ -0,0 +1,18 @@
+const ChevronUp = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="15"
+    height="15"
+    fill="none"
+    viewBox="0 0 15 15"
+  >
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M3.135 6.158a.5.5 0 0 1 .707-.023L7.5 9.565l3.658-3.43a.5.5 0 0 1 .684.73l-4 3.75a.5.5 0 0 1-.684 0l-4-3.75a.5.5 0 0 1-.023-.707"
+      clipRule="evenodd"
+    ></path>
+  </svg>
+);
+
+export default ChevronUp;
