e20fdde5b0357c50f6b67ac0b42413d1f60c38aa
Dalton Pettus <daltpettus@gmail.com>
2025-12-22 16:52:30 -0800

    remove log statement

--- src/utils/hooks/useSetCSSProperty.ts
+++ src/utils/hooks/useSetCSSProperty.ts
@@ -16,7 +16,6 @@ import { useCallback } from "react";
 export default function useSetCSSProperty(propertyName: string, value: string) {
   return useCallback((element: HTMLElement | null) => {
     if (element) {
-      console.log(`Setting ${propertyName} to ${value}`);
       element.style.setProperty(propertyName, value);
     }
   }, [propertyName, value]);
