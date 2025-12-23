1429bbe9ed3ab65ac29e7fd5b66c9de202f1ce0a
Dalton Pettus <daltpettus@gmail.com>
2025-12-22 16:15:21 -0800

    add documentation

--- src/utils/hooks/useSetCSSProperty.ts
+++ src/utils/hooks/useSetCSSProperty.ts
@@ -1,5 +1,18 @@
 import { useCallback } from "react";
 
+/**
+ * Hook that returns a callback to set a CSS property on an HTML element.
+ * @param propertyName - The name of the CSS property to set (e.g., '--my-color', 'background-color')
+ * @param value - The value to assign to the CSS property
+ * @returns A memoized callback function that accepts an HTMLElement and sets the specified CSS property to the given value
+ * @example
+ * ```typescript
+ * const setMyColor = useSetCSSProperty('--my-color', '#ff0000');
+ * useEffect(() => {
+ *   setMyColor(elementRef.current);
+ * }, [setMyColor]);
+ * ```
+ */
 export default function useSetCSSProperty(propertyName: string, value: string) {
   return useCallback((element: HTMLElement | null) => {
     if (element) {
