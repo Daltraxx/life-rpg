22d307e53038afea7b68bf6bcaf35c6a7d4e590c
Dalton Pettus <daltpettus@gmail.com>
2026-01-02 14:40:25 -0800

    add attibribute strength enum

--- src/app/ui/utils/types/AttributeStrength.ts
+++ src/app/ui/utils/types/AttributeStrength.ts
@@ -2,3 +2,9 @@
  * Represents internal representation of strength level of an attribute.
  */
 export type AttributeStrength = "normal" | "plus" | "plusPlus";
+
+export enum AttributeStrengthEnum {
+  Normal = "normal",
+  Plus = "plus",
+  PlusPlus = "plusPlus",
+}
