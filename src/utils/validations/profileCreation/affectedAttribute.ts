c4aaa7fd6bf4818e3c41655ca422de230634ac60
Dalton Pettus <daltpettus@gmail.com>
2026-01-02 15:59:27 -0800

    add trim to string validation

--- src/utils/validations/profileCreation/affectedAttribute.ts
+++ src/utils/validations/profileCreation/affectedAttribute.ts
@@ -3,6 +3,6 @@ import { AttributeStrengthValues } from "@/app/ui/utils/types/AttributeStrength"
 
 // TODO: Further refine schema
 export const AffectedAttributeSchema = z.object({
-  name: z.string().min(1, "Attribute name cannot be empty"),
+  name: z.string().trim().min(1, "Attribute name cannot be empty"),
   strength: z.enum(AttributeStrengthValues),
 });
