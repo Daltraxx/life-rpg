d25fea6497ea90c3bf74708d563e6cbb852f2852
Dalton Pettus <daltpettus@gmail.com>
2026-01-02 14:49:07 -0800

    add AttributeSchema

---
+++ src/utils/validations/profileCreation/attribute.ts
@@ -0,0 +1,7 @@
+import { z } from 'zod';
+
+// TODO: Further refine schema
+export const AttributeSchema = z.object({
+  name: z.string().min(1, 'Attribute name cannot be empty'),
+  order: z.number().int().nonnegative('Order must be a non-negative integer'),
+});
