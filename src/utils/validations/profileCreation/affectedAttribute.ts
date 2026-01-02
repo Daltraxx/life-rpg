82ad0fa3ff0729e130f1c23ee10d5c23d21aac6d
Dalton Pettus <daltpettus@gmail.com>
2026-01-02 15:58:12 -0800

    formatting

--- src/utils/validations/profileCreation/affectedAttribute.ts
+++ src/utils/validations/profileCreation/affectedAttribute.ts
@@ -1,8 +1,6 @@
 import { z } from "zod";
 import { AttributeStrengthValues } from "@/app/ui/utils/types/AttributeStrength";
 
-
-
 // TODO: Further refine schema
 export const AffectedAttributeSchema = z.object({
   name: z.string().min(1, "Attribute name cannot be empty"),
