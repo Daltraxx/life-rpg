d0675622fd383f56b4bbe8ed33ceed95907e99bc
Dalton Pettus <daltpettus@gmail.com>
2026-01-02 15:13:35 -0800

    add todo

--- src/utils/validations/profileCreation/profileCreation.ts
+++ src/utils/validations/profileCreation/profileCreation.ts
@@ -3,6 +3,7 @@ import { QuestSchema } from "@/utils/validations/profileCreation/quest";
 import { AttributeSchema } from "@/utils/validations/profileCreation/attribute";
 
 // TODO: Further refine schema
+// TODO: Ensure quest and attribute names are unique within their arrays
 export const ProfileCreationSchema = z.object({
   userId: z.uuid("Invalid user ID"),
   quests: z.array(QuestSchema).min(1, "At least one quest is required"),
