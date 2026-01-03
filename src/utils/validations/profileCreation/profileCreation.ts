5ce9e44cf98c1bcc4833ee56925b52971de826f4
Dalton Pettus <daltpettus@gmail.com>
2026-01-02 16:18:50 -0800

    implement helper

--- src/utils/validations/profileCreation/profileCreation.ts
+++ src/utils/validations/profileCreation/profileCreation.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { QuestSchema } from "@/utils/validations/profileCreation/quest";
 import { AttributeSchema } from "@/utils/validations/profileCreation/attribute";
+import ensureUniqueValue from '../../helpers/ensureUniqueValue';
 
 // TODO: Further refine schema
 // TODO: Ensure quest and attribute names are unique within their arrays
@@ -10,21 +11,15 @@ export const ProfileCreationSchema = z.object({
     .array(QuestSchema)
     .min(1, "At least one quest is required")
     .refine(
-      (quests) => {
-        const names = quests.map((q) => q.name);
-        return names.length === new Set(names).size;
-      },
+      (quests) => ensureUniqueValue(quests, 'name'),
       { message: "Quest names must be unique" }
     ),
   attributes: z
     .array(AttributeSchema)
     .min(1, "At least one attribute is required")
     .refine(
-      (quests) => {
-        const names = quests.map((q) => q.name);
-        return names.length === new Set(names).size;
-      },
-      { message: "Quest names must be unique" }
+      (attributes) => ensureUniqueValue(attributes, 'name'),
+      { message: "Attribute names must be unique" }
     ),
 });
 
