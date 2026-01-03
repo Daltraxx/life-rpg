7ea19065756e9ea2fd1b4a1e4dd4aecd242076df
Dalton Pettus <daltpettus@gmail.com>
2026-01-02 16:01:20 -0800

    add trim to name validation

--- src/utils/validations/profileCreation/attribute.ts
+++ src/utils/validations/profileCreation/attribute.ts
@@ -1,7 +1,7 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 // TODO: Further refine schema
 export const AttributeSchema = z.object({
-  name: z.string().min(1, 'Attribute name cannot be empty'),
-  order: z.number().int().nonnegative('Order must be a non-negative integer'),
+  name: z.string().trim().min(1, "Attribute name cannot be empty"),
+  order: z.number().int().nonnegative("Order must be a non-negative integer"),
 });
