afc43dbc477851c8d35e45d4fa4191547ff0f2ae
Dalton Pettus <daltpettus@gmail.com>
2026-01-02 16:06:54 -0800

    enforce name uniqueness for quests and affected attributes

--- src/utils/validations/profileCreation/profileCreation.ts
+++ src/utils/validations/profileCreation/profileCreation.ts
@@ -6,10 +6,26 @@ import { AttributeSchema } from "@/utils/validations/profileCreation/attribute";
 // TODO: Ensure quest and attribute names are unique within their arrays
 export const ProfileCreationSchema = z.object({
   userId: z.uuid("Invalid user ID"),
-  quests: z.array(QuestSchema).min(1, "At least one quest is required"),
+  quests: z
+    .array(QuestSchema)
+    .min(1, "At least one quest is required")
+    .refine(
+      (quests) => {
+        const names = quests.map((q) => q.name);
+        return names.length === new Set(names).size;
+      },
+      { message: "Quest names must be unique" }
+    ),
   attributes: z
     .array(AttributeSchema)
-    .min(1, "At least one attribute is required"),
+    .min(1, "At least one attribute is required")
+    .refine(
+      (quests) => {
+        const names = quests.map((q) => q.name);
+        return names.length === new Set(names).size;
+      },
+      { message: "Quest names must be unique" }
+    ),
 });
 
 export type ProfileCreationState = {
