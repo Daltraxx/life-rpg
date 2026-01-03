7b1412ce43b60656705567685ae52a2718f98374
Dalton Pettus <daltpettus@gmail.com>
2026-01-02 16:02:20 -0800

    add max char to attr name

--- src/utils/validations/profileCreation/attribute.ts
+++ src/utils/validations/profileCreation/attribute.ts
@@ -2,6 +2,10 @@ import { z } from "zod";
 
 // TODO: Further refine schema
 export const AttributeSchema = z.object({
-  name: z.string().trim().min(1, "Attribute name cannot be empty"),
+  name: z
+    .string()
+    .trim()
+    .min(1, "Attribute name cannot be empty")
+    .max(30, "Attribute name cannot exceed 30 characters"),
   order: z.number().int().nonnegative("Order must be a non-negative integer"),
 });
