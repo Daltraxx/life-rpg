8e8170eba46251b09943efb834d90257c72c1dfe
Dalton Pettus <daltpettus@gmail.com>
2026-01-04 14:57:11 -0800

    update documentation

--- src/utils/types/profile_transaction/createProfileTransactionDataShapes.ts
+++ src/utils/types/profile_transaction/createProfileTransactionDataShapes.ts
@@ -1,16 +1,23 @@
 // Contains data shapes used in data preparation for the create_profile_transaction supabase function.
 
+/**
+ * Represents attribute data necessary for a profile transaction rpc call.
+ */
 export interface CreateProfileTransactionAttributes {
   name: string;
   position: number;
 }
-
+/**
+ * Represents quest data necessary for a profile transaction rpc call.
+ */
 export interface CreateProfileTransactionQuests {
   name: string;
   experience_share: number;
   position: number;
 }
 
+/** Represents quest-attribute relationship data necessary for a profile transaction rpc call.
+ */
 export interface CreateProfileTransactionQuestsAttributes {
   quest_name: string;
   attribute_name: string;
