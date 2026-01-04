27843fdab106f5c569d4c9a356ca65c9d5173ca8
Dalton Pettus <daltpettus@gmail.com>
2026-01-04 14:49:10 -0800

    give max amount to quests and attributes

--- src/utils/validations/profileCreation/profileCreation.ts
+++ src/utils/validations/profileCreation/profileCreation.ts
@@ -1,24 +1,24 @@
 import { z } from "zod";
 import { QuestSchema } from "@/utils/validations/profileCreation/quest";
 import { AttributeSchema } from "@/utils/validations/profileCreation/attribute";
-import hasUniqueValues from '@/utils/helpers/hasUniqueValues';
+import hasUniqueValues from "@/utils/helpers/hasUniqueValues";
 
 export const ProfileCreationSchema = z.object({
   userId: z.uuid("Invalid user ID"),
   quests: z
     .array(QuestSchema)
     .min(1, "At least one quest is required")
-    .refine(
-      (quests) => hasUniqueValues(quests, 'name'),
-      { message: "Quest names must be unique" }
-    ),
+    .max(50, "No more than 50 quests are allowed")
+    .refine((quests) => hasUniqueValues(quests, "name"), {
+      message: "Quest names must be unique",
+    }),
   attributes: z
     .array(AttributeSchema)
     .min(1, "At least one attribute is required")
-    .refine(
-      (attributes) => hasUniqueValues(attributes, 'name'),
-      { message: "Attribute names must be unique" }
-    ),
+    .max(50, "No more than 50 attributes are allowed")
+    .refine((attributes) => hasUniqueValues(attributes, "name"), {
+      message: "Attribute names must be unique",
+    }),
 });
 
 export type ProfileCreationState = {
