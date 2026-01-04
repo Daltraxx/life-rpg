511b960cb36711c1cb0ce43a11e85340433b0030
Dalton Pettus <daltpettus@gmail.com>
2026-01-04 13:26:58 -0800

    ensure all affected attribute names are unique

--- src/utils/validations/profileCreation/quest.ts
+++ src/utils/validations/profileCreation/quest.ts
@@ -1,12 +1,23 @@
 import { z } from "zod";
 import { AffectedAttributeSchema } from "@/utils/validations/profileCreation/affectedAttribute";
+import hasUniqueValues from "@/utils/helpers/hasUniqueValues";
 
 // TODO: Further refine schema
 export const QuestSchema = z.object({
-  name: z.string().trim().min(1, "Quest name cannot be empty").max(50, "Quest name cannot exceed 50 characters"),
+  name: z
+    .string()
+    .trim()
+    .min(1, "Quest name cannot be empty")
+    .max(50, "Quest name cannot exceed 50 characters"),
   affectedAttributes: z
     .array(AffectedAttributeSchema)
-    .min(1, "At least one affected attribute is required"),
+    .min(1, "At least one affected attribute is required")
+    .refine(
+      (attributes) => {
+        return hasUniqueValues(attributes, "name");
+      },
+      { message: "Affected attribute names must be unique" }
+    ),
   order: z.number().int().nonnegative("Order must be a non-negative integer"),
   experiencePointValue: z
     .number()
