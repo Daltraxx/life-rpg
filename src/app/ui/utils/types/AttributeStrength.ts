ca7e41954fb96f2756fa74407c6b16952be2a54e
Dalton Pettus <daltpettus@gmail.com>
2026-01-04 13:35:26 -0800

    derive the type from the constant for type safety and maintenance

--- src/app/ui/utils/types/AttributeStrength.ts
+++ src/app/ui/utils/types/AttributeStrength.ts
@@ -1,6 +1,6 @@
+export const AttributeStrengthValues = ["normal", "plus", "plusPlus"] as const;
+
 /**
  * Represents internal representation of strength level of an attribute.
  */
-export type AttributeStrength = "normal" | "plus" | "plusPlus";
-
-export const AttributeStrengthValues = ["normal", "plus", "plusPlus"] as const;
+export type AttributeStrength = (typeof AttributeStrengthValues)[number];
