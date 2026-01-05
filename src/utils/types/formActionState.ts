8a02b5e0d19318809d50f450fbcaa6f452061979
Dalton Pettus <daltpettus@gmail.com>
2026-01-05 11:10:05 -0800

    switch from null to undefined

--- src/utils/types/formActionState.ts
+++ src/utils/types/formActionState.ts
@@ -5,10 +5,10 @@
  *
  * @property {Record<string, string[]>} errors - A map of field names to their corresponding validation error messages.
  *           Each field can have multiple error messages associated with it.
- * @property {string | null} message - An optional message providing feedback about the form action result.
- *           Can be null if no message is available.
+ * @property {string | undefined} message - An optional message providing feedback about the form action result.
+ *           Can be undefined if no message is available.
  */
 export interface SimpleFormActionState {
   errors: Record<string, string[]>;
-  message: string | null;
+  message: string | undefined;
 }
