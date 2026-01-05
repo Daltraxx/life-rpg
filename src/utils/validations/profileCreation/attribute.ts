12915a397dba958019cddd13befa0d4f1e63afc6
Dalton Pettus <daltpettus@gmail.com>
2026-01-05 14:29:18 -0800

    add max to order number

--- src/utils/validations/profileCreation/attribute.ts
+++ src/utils/validations/profileCreation/attribute.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { MAX_ATTRIBUTES_ALLOWED } from "@/app/ui/utils/constants/gameConstants";
 
 // TODO: Further refine schema (e.g., restrict names to certain characters)
 export const AttributeSchema = z.object({
@@ -7,7 +8,13 @@ export const AttributeSchema = z.object({
     .trim()
     .min(1, "Attribute name cannot be empty")
     .max(30, "Attribute name cannot exceed 30 characters"),
-  order: z.int("Order must be an integer").nonnegative("Order must be a non-negative integer"),
+  order: z
+    .int("Order must be an integer")
+    .nonnegative("Order must be a non-negative integer")
+    .max(
+      MAX_ATTRIBUTES_ALLOWED - 1,
+      `Order must be less than ${MAX_ATTRIBUTES_ALLOWED}`
+    ),
 });
 
 export type Attribute = z.infer<typeof AttributeSchema>;
