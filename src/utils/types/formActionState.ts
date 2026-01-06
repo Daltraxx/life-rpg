d2b009eaee085c88a1f3c2f30713d3b13e8e4514
Dalton Pettus <daltpettus@gmail.com>
2026-01-06 13:50:48 -0800

    update doc

--- src/utils/types/formActionState.ts
+++ src/utils/types/formActionState.ts
@@ -3,10 +3,9 @@
  *
  * @interface SimpleFormActionState
  *
- * @property {Record<string, string[]>} errors - A map of field names to their corresponding validation error messages.
+ * @property {Record<string, string[]>} [errors] - A map of field names to their corresponding validation error messages.
  *           Each field can have multiple error messages associated with it.
- * @property {string | undefined} message - An optional message providing feedback about the form action result.
- *           Can be undefined if no message is available.
+ * @property {string} [message] - An optional message providing feedback about the form action result.
  */
 export interface SimpleFormActionState {
   errors?: Record<string, string[]>;
