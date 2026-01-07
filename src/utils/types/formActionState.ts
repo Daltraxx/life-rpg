91322c1e82d4d7acccacf703374fdba963fcb286
Dalton Pettus <daltpettus@gmail.com>
2026-01-07 14:54:45 -0800

    add success option to SimpleFormActionState

--- src/utils/types/formActionState.ts
+++ src/utils/types/formActionState.ts
@@ -6,8 +6,10 @@
  * @property {Record<string, string[]>} [errors] - A map of field names to their corresponding validation error messages.
  *           Each field can have multiple error messages associated with it.
  * @property {string} [message] - An optional message providing feedback about the form action result.
+ * @property {boolean} [success] - An optional flag indicating whether the form action was successful.
  */
 export interface SimpleFormActionState {
   errors?: Record<string, string[]>;
   message?: string;
+  success?: boolean;
 }
