9def6ac8387072635f2521bf3f4311cb2b963d7a
Dalton Pettus <daltpettus@gmail.com>
2026-01-10 07:51:53 -0800

    formatting

--- src/utils/actions/createProfile.ts
+++ src/utils/actions/createProfile.ts
@@ -56,7 +56,7 @@ export default async function createProfile(
   formData: FormData
 ): Promise<ProfileCreationState | void> {
   const supabase = await createSupabaseServerClient();
-  
+
   const {
     data: { user },
     error: authError,
