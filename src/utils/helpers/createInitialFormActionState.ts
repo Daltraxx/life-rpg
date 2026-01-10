926f0e4aeb5dc9da2155fcf02270f3a7a1accdc8
Dalton Pettus <daltpettus@gmail.com>
2026-01-09 16:04:21 -0800

    clarify doc

--- src/utils/helpers/createInitialFormActionState.ts
+++ src/utils/helpers/createInitialFormActionState.ts
@@ -1,7 +1,7 @@
 import type { SimpleFormActionState } from "@/utils/types/formActionState";
 
 /**
- * Creates an initial form action state object with empty errors and no message.
+ * Creates an initial form action state object with empty errors and no message field set.
  *
  * @returns {Object} An object containing:
  * - `errors`: An empty object for storing form field errors
