fc5fe641b2e61d4302fe65de9d6aeeb5bfa13b6a
Dalton Pettus <daltpettus@gmail.com>
2026-04-19 16:33:35 -0700

    add menu button to header layout

--- src/app/ui/account-creation/Header/Header.tsx
+++ src/app/ui/account-creation/Header/Header.tsx
@@ -3,6 +3,7 @@ import Bounded from "@/app/ui/JSXWrappers/Bounded";
 import Heading from "@/app/ui/JSXWrappers/Heading/Heading";
 import styles from "./styles.module.css";
 import { JSX } from "react";
+import { Button } from "@/app/ui/JSXWrappers/TextWrappers/TextWrappers";
 
 export default function Header(): JSX.Element {
   return (
@@ -11,6 +12,7 @@ export default function Header(): JSX.Element {
       outerClassName={styles.boundedContainer}
       innerClassName={styles.contentContainer}
     >
+      <Button className={styles.menuButton} size="30">MENU</Button>
       <Link href="/" className={styles.brandingLink}>
         <Heading as="h2" size="72" className={styles.branding}>
           LifeRPG
