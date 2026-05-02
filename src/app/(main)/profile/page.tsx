624352e4e20ef90f124788983f1948872623ad61
Dalton Pettus <daltpettus@gmail.com>
2026-05-01 19:43:36 -0700

    add profile page.tsx starter code

---
+++ src/app/(main)/profile/page.tsx
@@ -0,0 +1,10 @@
+import { type Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Profile",
+  description:
+    "View your LifeRPG profile, view your progress, and manage your quests for the day.",
+};
+
+export default function ProfilePage() {
+  return <div>Profile Page</div>;
+}
