4f8ac16f368eb5d4218e1859b670f45682c9dba6
Dalton Pettus <daltpettus@gmail.com>
2026-05-06 20:52:50 -0700

    pass quests to QuestBoard

--- src/app/ui/Profile/DailyQuests/DailyQuests.tsx
+++ src/app/ui/Profile/DailyQuests/DailyQuests.tsx
@@ -3,7 +3,6 @@ import getQuests from "@/app/queries/server/getQuests";
 
 export default async function DailyQuests() {
   const quests = await getQuests();
-  console.log("Fetched quests:", quests);
   return (
     <div>
       <h1>Welcome, user.</h1>
