156af6519a4a5bb4711868123b652629289a4238
Dalton Pettus <daltpettus@gmail.com>
2026-05-10 18:35:50 -0700

    make browser version of query

---
+++ src/app/queries/client/getQuestCompletionStatus.ts
@@ -0,0 +1,34 @@
+import { createSupabaseBrowserClient } from "@/utils/supabase/client";
+
+export default async function getQuestCompletionStatus(
+  questId: string,
+): Promise<boolean> {
+  const supabase = createSupabaseBrowserClient();
+
+  const { data, error } = await supabase
+    .from("quest_completions")
+    .select("completed_at")
+    .eq("quest_id", questId)
+    .order("completed_at", { ascending: false })
+    .limit(1);
+
+  if (error) {
+    console.error("Error fetching quest completion status:", error);
+    return false;
+  }
+
+  const today = new Date();
+  if (data.length > 0) {
+    const lastCompletionDate = new Date(data[0].completed_at);
+    // Check if the last completion date is today    
+    if (
+      lastCompletionDate.getFullYear() === today.getFullYear() &&
+      lastCompletionDate.getMonth() === today.getMonth() &&
+      lastCompletionDate.getDate() === today.getDate()
+    ) {
+      return true;
+    }
+  }
+
+  return false;
+}
