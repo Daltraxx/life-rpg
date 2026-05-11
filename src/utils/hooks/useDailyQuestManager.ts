67a644c98604b6c9b322d8b0482313de51fd9312
Dalton Pettus <daltpettus@gmail.com>
2026-05-10 19:33:04 -0700

    write initial code for DailyQuestManager reducer custom hook

---
+++ src/utils/hooks/useDailyQuestManager.ts
@@ -0,0 +1,67 @@
+import { useReducer } from 'react';
+import { DailyQuest } from '../types/DailyQuest';
+
+interface DailyQuestState {
+  dailyQuests: DailyQuest[];
+}
+
+type DailyQuestAction =
+  | { type: "completeQuest"; questId: number }
+  | { type: "undoCompleteQuest"; questId: number };
+
+function dailyQuestReducer(
+  state: DailyQuestState,
+  action: DailyQuestAction,
+): DailyQuestState {
+  switch (action.type) {
+    case "completeQuest": {
+      const { questId } = action;
+      // Write to database to mark quest as completed for the day (TODO: implement)
+      return {
+        ...state,
+        dailyQuests: state.dailyQuests.map((quest) =>
+          quest.id === questId ? { ...quest, isCompleted: true } : quest,
+        ),
+      }
+    }
+    case "undoCompleteQuest": {
+      const { questId } = action;
+      // Write to database to undo quest completion for the day (TODO: implement)
+      return {
+        ...state,
+        dailyQuests: state.dailyQuests.map((quest) =>
+          quest.id === questId ? { ...quest, isCompleted: false } : quest,
+        ),
+      }
+    }
+    default:
+      throw new Error(`Unhandled action type in dailyQuestReducer.`);
+  }
+}
+
+interface DailyQuestManager { 
+  dailyQuests: DailyQuest[];
+  actions: {
+    completeQuest: (questId: number) => void;
+    undoCompleteQuest: (questId: number) => void;
+  };
+}
+
+export default function useDailyQuestManager(dailyQuests: DailyQuest[]): DailyQuestManager { 
+  const [state, dispatch] = useReducer(dailyQuestReducer, { dailyQuests });
+
+  const completeQuest = (questId: number) => {
+    dispatch({ type: "completeQuest", questId });
+  };
+  const undoCompleteQuest = (questId: number) => {
+    dispatch({ type: "undoCompleteQuest", questId });
+  };
+
+  return {
+    dailyQuests: state.dailyQuests,
+    actions: {
+      completeQuest,
+      undoCompleteQuest,
+    },
+  };
+}
