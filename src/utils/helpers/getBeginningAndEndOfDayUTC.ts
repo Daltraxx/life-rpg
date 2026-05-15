b49522440e38282e339bd15df8adfa67dea207e1
Dalton Pettus <daltpettus@gmail.com>
2026-05-14 19:03:35 -0700

    write function for getting beginning and end of users day translated to utc

---
+++ src/utils/helpers/getBeginningAndEndOfDayUTC.ts
@@ -0,0 +1,20 @@
+import { TZDate } from "@date-fns/tz";
+import { endOfDay, startOfDay } from "date-fns";
+
+export interface UTCDayBoundaries {
+  beginningOfDayUTC: string;
+  endOfDayUTC: string;
+}
+export default function getBeginningAndEndOfDayUTC(userTimezone: string): UTCDayBoundaries {
+  const nowInUserTZ = new TZDate(new Date(), userTimezone);
+
+  // Start/end of the current day in the user's timezone
+  const beginningOfDayUserTZ = startOfDay(nowInUserTZ);
+  const endOfDayUserTZ = endOfDay(nowInUserTZ);
+
+  // UTC timestamps for the start and end of the day in the user's timezone
+  const beginningOfDayUTC = beginningOfDayUserTZ.toISOString();
+  const endOfDayUTC = endOfDayUserTZ.toISOString();
+
+  return { beginningOfDayUTC, endOfDayUTC };
+}
