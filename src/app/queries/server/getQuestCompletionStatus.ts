a1152d3925ded943d74271679ec3ee13c8a28715
Dalton Pettus <daltpettus@gmail.com>
2026-05-14 20:36:50 -0700

    fix getQuestCompletionStatus

--- src/app/queries/server/getQuestCompletionStatus.ts
+++ src/app/queries/server/getQuestCompletionStatus.ts
@@ -1,34 +1,45 @@
 import { createSupabaseServerClient } from "@/utils/supabase/server";
+import isQuestCompletedToday from "./helpers/isQuestCompletedToday";
 
+/**
+ * Retrieves the completion status of a quest for the current user.
+ *
+ * @param questId - The ID of the quest to check
+ * @param userID - Optional user ID. If not provided, the current authenticated user is used
+ * @returns A promise that resolves to `true` if the quest was completed today, `false` otherwise
+ * @throws Will throw an error if there's an issue fetching quest completion data or if the user is not authenticated
+ */
 export default async function getQuestCompletionStatus(
-  questId: string,
+  questId: number,
+  userID?: string,
 ): Promise<boolean> {
   const supabase = await createSupabaseServerClient();
 
   const { data, error } = await supabase
     .from("quest_completions")
-    .select("completed_at")
+    .select("completed_at, is_resolved")
     .eq("quest_id", questId)
     .order("completed_at", { ascending: false })
     .limit(1);
 
   if (error) {
-    console.error("Error fetching quest completion status:", error);
-    return false;
+    throw new Error("Error fetching quest completion status: " + error.message);
   }
 
-  const today = new Date();
-  if (data.length > 0) {
-    const lastCompletionDate = new Date(data[0].completed_at);
-    // Check if the last completion date is today    
-    if (
-      lastCompletionDate.getFullYear() === today.getFullYear() &&
-      lastCompletionDate.getMonth() === today.getMonth() &&
-      lastCompletionDate.getDate() === today.getDate()
-    ) {
-      return true;
+  let userId = userID;
+  if (!userId) {
+    const { data: userData, error: userError } = await supabase.auth.getUser();
+    if (userError || !userData.user) {
+      console.error("Error fetching user ID:", userError);
+      throw new Error("User not authenticated");
     }
+    userId = userData.user.id;
   }
 
-  return false;
+  const isCompletedToday = await isQuestCompletedToday({
+    userId: userId,
+    latestCompletion: data[0],
+  });
+
+  return isCompletedToday;
 }
