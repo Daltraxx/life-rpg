f92105913e692a65041f3712b87200661b3f65c8
Dalton Pettus <daltpettus@gmail.com>
2026-05-19 15:42:21 -0700

    add text for display

--- src/app/ui/QuestBoard/DailyExperienceDisplay/DailyExperienceDisplay.tsx
+++ src/app/ui/QuestBoard/DailyExperienceDisplay/DailyExperienceDisplay.tsx
@@ -1,5 +1,6 @@
 import { DailyQuestManager } from "@/utils/hooks/useDailyQuestManager";
 import Heading from "@/app/ui/JSXWrappers/Heading/Heading";
+import { Span,  } from "@/app/ui/JSXWrappers/TextWrappers/TextWrappers";
 
 export interface DailyExperienceDisplayProps {
   dailyQuestManager: DailyQuestManager;
@@ -7,16 +8,28 @@ export interface DailyExperienceDisplayProps {
 
 export default function DailyExperienceDisplay({
   dailyQuestManager,
-}: DailyExperienceDisplayProps) { 
+}: DailyExperienceDisplayProps) {
+  const { totalPoints, totalBonusPoints } = dailyQuestManager;
   return (
     <section>
       <Heading as="h3" size="30" color="background">
         Total Experience Earned Today:
       </Heading>
       <div>
-        <div><p>{dailyQuestManager.totalExperience} + </p></div>
-        <div></div>
+        <div>
+          <Span size="24" color="background">
+            {totalBonusPoints > 0
+              ? `${totalPoints} + ${totalBonusPoints}`
+              : `${totalPoints}`}
+          </Span>
+          <Span size="24" color="background">
+            100
+          </Span>
+        </div>
       </div>
+      <Span size="36" color="background">
+        EXP!
+      </Span>
     </section>
   );
 }
