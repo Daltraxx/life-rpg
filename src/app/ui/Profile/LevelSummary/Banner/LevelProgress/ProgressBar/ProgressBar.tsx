c4576a914110842fe4dfa61f72c4ce0653e92511
Dalton Pettus <daltpettus@gmail.com>
2026-05-20 20:22:10 -0700

    create skeleton of progress bar

--- src/app/ui/Profile/LevelSummary/Banner/LevelProgress/ProgressBar/ProgressBar.tsx
+++ src/app/ui/Profile/LevelSummary/Banner/LevelProgress/ProgressBar/ProgressBar.tsx
@@ -1,3 +1,10 @@
+import styles from "./styles.module.css"
+
 export default function ProgressBar() {
-  return <div>ProgressBar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.progress}>
+      </div>
+    </div>
+  );
 }
