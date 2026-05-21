2d116e8b2110cf55c41066ac4b4e0aa9e903b97b
Dalton Pettus <daltpettus@gmail.com>
2026-05-20 20:12:37 -0700

    start creating LevelProgress component

--- src/app/ui/Profile/LevelSummary/Banner/Banner.tsx
+++ src/app/ui/Profile/LevelSummary/Banner/Banner.tsx
@@ -1,9 +1,11 @@
 import Avatar from "./Avatar/Avatar";
+import LevelProgress from "./LevelProgress/LevelProgress";
 
 export default function Banner() {
   return (
     <div>
       <Avatar />
+      <LevelProgress />
     </div>
   );
 }
---
+++ src/app/ui/Profile/LevelSummary/Banner/LevelProgress/LevelProgress.tsx
@@ -0,0 +1,9 @@
+import Heading from "@/app/ui/JSXWrappers/Heading/Heading"
+
+export default function LevelProgress() {
+  return (
+    <div>
+      <Heading color="blue-700" size="48-responsive">Level _</Heading>
+    </div>
+  )
+}
