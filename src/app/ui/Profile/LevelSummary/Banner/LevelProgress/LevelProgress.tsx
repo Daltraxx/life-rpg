031fef9bacd5ed998c21d88f75b729458ad8c220
Dalton Pettus <daltpettus@gmail.com>
2026-05-20 20:15:57 -0700

    prepare for progress bar

--- src/app/ui/Profile/LevelSummary/Banner/LevelProgress/LevelProgress.tsx
+++ src/app/ui/Profile/LevelSummary/Banner/LevelProgress/LevelProgress.tsx
@@ -1,9 +1,14 @@
-import Heading from "@/app/ui/JSXWrappers/Heading/Heading"
+import Heading from "@/app/ui/JSXWrappers/Heading/Heading";
+import ProgressBar from "./ProgressBar/ProgressBar";
+import styles from "./styles.module.css";
 
 export default function LevelProgress() {
   return (
     <div>
-      <Heading color="blue-700" size="48-responsive">Level _</Heading>
+      <Heading color="blue-700" size="48-responsive">
+        Level _
+      </Heading>
+      <ProgressBar />
     </div>
-  )
+  );
 }
