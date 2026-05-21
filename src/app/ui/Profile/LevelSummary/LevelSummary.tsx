4d3b40896db2978ee4521c93944d7639cab9e430
Dalton Pettus <daltpettus@gmail.com>
2026-05-20 19:42:13 -0700

    prepare for level summary component

---
+++ src/app/ui/Profile/LevelSummary/LevelSummary.tsx
@@ -0,0 +1,3 @@
+export default function LevelSummary() {
+  return <div>LevelSummary</div>;
+}
--- src/app/ui/Profile/Profile.tsx
+++ src/app/ui/Profile/Profile.tsx
@@ -1,10 +1,17 @@
 import Bounded from "@/app/ui/JSXWrappers/Bounded";
 import DailyQuests from "./DailyQuests/DailyQuests";
+import LevelSummary from "./LevelSummary/LevelSummary";
+import styles from "./styles.module.css";
 
 export default function Profile() {
   return (
-    <Bounded>
-      <DailyQuests />
-    </Bounded>
+    <>
+      <Bounded outerClassName={styles.dailyQuestsContainer}>
+        <DailyQuests />
+      </Bounded>
+      <Bounded outerClassName={styles.levelSummaryContainer}>
+        <LevelSummary />
+      </Bounded>
+    </>
   );
 }
