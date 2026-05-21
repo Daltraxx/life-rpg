f0861b5b17d9d15599fefc643eeda82dbaca6203
Dalton Pettus <daltpettus@gmail.com>
2026-05-20 20:42:41 -0700

    build beginnings of working progress bar

--- src/app/ui/Profile/LevelSummary/Banner/LevelProgress/ProgressBar/ProgressBar.tsx
+++ src/app/ui/Profile/LevelSummary/Banner/LevelProgress/ProgressBar/ProgressBar.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import styles from "./styles.module.css"
 
 export default function ProgressBar() {
+  const left = 100;
+  const right = 200;
+  const value = 160;
+  const progressWidth = ((value - left) / (right - left)) * 100;
   return (
     <div className={styles.container}>
-      <div className={styles.progress}>
+      <div className={styles.progress} style={{ width: `${progressWidth}%` }}>
       </div>
     </div>
   );
