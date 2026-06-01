3d2f9d4ec6862d13252fda09ca804d64338a2610
Dalton Pettus <daltpettus@gmail.com>
2026-06-01 15:00:59 -0700

    define AttributeProgress object type

---
+++ src/utils/types/AttributeProgress.ts
@@ -0,0 +1,17 @@
+/**
+ * Represents the progress of a specific attribute for a user, 
+ * including the current level, experience points, and the experience 
+ * required for the current and next levels.
+ * @property {string} attributeName - The name of the attribute (e.g., "Strength", "Intelligence").
+ * @property {number} experience - The total experience points accumulated for this attribute.
+ * @property {number} level - The current level of the attribute based on the accumulated experience.
+ * @property {number} levelStart - The experience points required to reach the current level.
+ * @property {number} levelEnd - The experience points required to reach the next level.
+ */
+export interface AttributeProgress {
+  attributeName: string;
+  experience: number;
+  level: number;
+  levelStart: number;
+  levelEnd: number;
+}
