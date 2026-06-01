276833c5f045054a1c93b4b92e9e0f9a878980ee
Dalton Pettus <daltpettus@gmail.com>
2026-06-01 15:03:33 -0700

    complete query function for getting user progress

--- src/app/queries/server/getUserProgress.ts
+++ src/app/queries/server/getUserProgress.ts
@@ -1,6 +1,22 @@
+import getUserLevelProgress, {
+  UserLevelProgress,
+} from "@/utils/helpers/getUserLevelProgress";
+import getAttributeLevelProgress, {
+  AttributeLevelProgress,
+} from "@/utils/helpers/getAttributeLevelProgress";
 import { createSupabaseServerClient } from "@/utils/supabase/server";
+import { UserProgress } from "@/utils/types/UserProgress";
 
-export default async function getUserProgress(userId: string) {
+/**
+ * Fetches and calculates the progress data for a user.
+ *
+ * @param userId - The unique identifier of the user.
+ * @returns A promise that resolves to the user's progress data, including level, experience, purpose, and attribute progress.
+ * @throws Error if the user data cannot be fetched from the database.
+ */
+export default async function getUserProgress(
+  userId: string,
+): Promise<UserProgress> {
   const supabase = await createSupabaseServerClient();
 
   const { data, error } = await supabase
@@ -29,6 +45,28 @@ export default async function getUserProgress(userId: string) {
     throw new Error("Error fetching user progress");
   }
 
-  console.log("User progress data:", data);
-  return data;
+  const userLevelProgress: UserLevelProgress = getUserLevelProgress(
+    data.experience,
+  );
+
+  const userProgress: UserProgress = {
+    level: userLevelProgress.level,
+    experience: data.experience,
+    purpose: data.purpose ?? "Add your purpose statement in your profile!",
+    levelStart: userLevelProgress.levelExperienceStart,
+    levelEnd: userLevelProgress.levelExperienceEnd,
+    attributes: data.attributes.map((attr) => {
+      const attributeLevelProgress: AttributeLevelProgress =
+        getAttributeLevelProgress(attr.experience);
+      return {
+        attributeName: attr.name,
+        experience: attr.experience,
+        level: attributeLevelProgress.level,
+        levelStart: attributeLevelProgress.levelExperienceStart,
+        levelEnd: attributeLevelProgress.levelExperienceEnd,
+      };
+    }),
+  };
+
+  return userProgress;
 }
