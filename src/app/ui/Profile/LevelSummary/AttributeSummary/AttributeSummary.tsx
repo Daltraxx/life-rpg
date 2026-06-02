31afe21ad24813711a488a3d45051f79072706b3
Dalton Pettus <daltpettus@gmail.com>
2026-06-02 11:59:12 -0700

    style attributes summary for small screens

--- src/app/ui/Profile/LevelSummary/AttributeSummary/AttributeSummary.tsx
+++ src/app/ui/Profile/LevelSummary/AttributeSummary/AttributeSummary.tsx
@@ -1,17 +1,39 @@
 import Heading from "@/app/ui/JSXWrappers/Heading/Heading";
+import { Span } from "@/app/ui/JSXWrappers/TextWrappers/TextWrappers";
 import { UserProgress } from "@/utils/types/UserProgress";
+import ProgressBar from "../Banner/LevelProgress/ProgressBar/ProgressBar";
+import styles from "./styles.module.css";
 
 export default function AttributeSummary({
   userProgress,
 }: {
   userProgress: UserProgress;
 }) {
+  const { attributes } = userProgress;
   return (
-    <section>
+    <section className={styles.container}>
       <Heading as="h2" size="30-responsive" color="blue-700">
         Attributes
       </Heading>
-      <ul></ul>
+      <ul className={styles.list}>
+        {attributes.map((attr) => (
+          <li key={attr.attributeId} className={styles.listItem}>
+            <Span
+              className={styles.attributeName}
+              color="blue-700"
+              size="20-responsive"
+            >
+              {attr.attributeName} - {attr.level}
+            </Span>
+            <ProgressBar
+              size="small"
+              start={attr.levelStart}
+              end={attr.levelEnd}
+              current={attr.experience}
+            />
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
