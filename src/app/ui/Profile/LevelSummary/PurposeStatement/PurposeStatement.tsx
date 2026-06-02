23355a5036f4bbcfe7cf3823cf288515dbe27bf0
Dalton Pettus <daltpettus@gmail.com>
2026-06-02 13:04:40 -0700

    add gap between heading and paragraph

--- src/app/ui/Profile/LevelSummary/PurposeStatement/PurposeStatement.tsx
+++ src/app/ui/Profile/LevelSummary/PurposeStatement/PurposeStatement.tsx
@@ -1,5 +1,7 @@
+import clsx from "clsx";
 import Heading from "../../../JSXWrappers/Heading/Heading";
 import { Paragraph } from "../../../JSXWrappers/TextWrappers/TextWrappers";
+import styles from "./styles.module.css";
 
 type PurposeStatementProps = {
   purposeText: string;
@@ -8,7 +10,7 @@ type PurposeStatementProps = {
 
 export default function PurposeStatement({ purposeText, className }: PurposeStatementProps) {
   return (
-    <section className={className}>
+    <section className={clsx(styles.container, className)}>
       <Heading as="h3" size="30" color="blue-700">
         Purpose
       </Heading>
