a649f7b801d8080965b83b5080aa610177f18c17
Dalton Pettus <daltpettus@gmail.com>
2026-06-03 18:04:43 -0700

    refactor

--- src/utils/types/accountSetup/profile_transaction/createProfileTransactionDataShapes.ts
+++ src/utils/types/accountSetup/profile_transaction/createProfileTransactionDataShapes.ts
@@ -2,10 +2,10 @@
 
 /**
  * Represents the complete data shape for the create_profile_transaction rpc call.
- * @param p_user_id - The id of the user for which the profile is being created.
- * @param p_attributes - An array of attribute objects representing the attributes to be created for the profile.
- * @param p_quests - An array of quest objects representing the quests to be created for the profile.
- * @param p_quests_attributes - An array of quest-attribute relationship objects representing the relationships between quests and attributes for the profile.
+ * @property {string} p_user_id - The id of the user for which the profile is being created.
+ * @property {CreateProfileTransactionAttributes[]} p_attributes - An array of attribute objects representing the attributes to be created for the profile.
+ * @property {CreateProfileTransactionQuests[]} p_quests - An array of quest objects representing the quests to be created for the profile.
+ * @property {CreateProfileTransactionQuestsAttributes[]} p_quests_attributes - An array of quest-attribute relationship objects representing the relationships between quests and attributes for the profile.
  */
 export interface CreateProfileTransactionDataShapes {
   p_user_id: string;
@@ -16,8 +16,8 @@ export interface CreateProfileTransactionDataShapes {
 
 /**
  * Represents attribute data necessary for a profile transaction rpc call.
- * @param name - The name of the attribute.
- * @param position - The 0-indexed position of the attribute in the ordered list.
+ * @property {string} name - The name of the attribute.
+ * @property {number} position - The 0-indexed position of the attribute in the ordered list.
  */
 export interface CreateProfileTransactionAttributes {
   name: string;
@@ -25,9 +25,9 @@ export interface CreateProfileTransactionAttributes {
 }
 /**
  * Represents quest data necessary for a profile transaction rpc call.
- * @param name - The name of the quest.
- * @param experience_share - The share (integer between 0-100 inclusive) of daily possible experience points allocated to the quest.
- * @param position - The 0-indexed position of the quest in the ordered list.
+ * @property {string} name - The name of the quest.
+ * @property {number} experience_share - The share (integer between 0-100 inclusive) of daily possible experience points allocated to the quest.
+ * @property {number} position - The 0-indexed position of the quest in the ordered list.
  */
 export interface CreateProfileTransactionQuests {
   name: string;
@@ -37,9 +37,9 @@ export interface CreateProfileTransactionQuests {
 
 /**
  * Represents quest-attribute relationship data necessary for a profile transaction rpc call.
- * @param quest_name - The name of the quest.
- * @param attribute_name - The name of the attribute.
- * @param attribute_power - The integer indicator (1-3) of the experience points given to the attribute upon the associated quests completion.
+ * @property {string} quest_name - The name of the quest.
+ * @property {string} attribute_name - The name of the attribute.
+ * @property {number} attribute_power - The integer indicator (1-3) of the experience points given to the attribute upon the associated quests completion.
  */
   export interface CreateProfileTransactionQuestsAttributes {
   quest_name: string;
