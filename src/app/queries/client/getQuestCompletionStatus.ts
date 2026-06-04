bb0c4589e4d92f3b8e3c358aedc23b3543ff9af2
Dalton Pettus <daltpettus@gmail.com>
2026-06-04 13:25:06 -0700

    remove userId parameter and get userId from session

--- src/app/queries/client/getQuestCompletionStatus.ts
+++ src/app/queries/client/getQuestCompletionStatus.ts
@@ -5,13 +5,11 @@ import isQuestCompletedToday from "./helpers/isQuestCompletedToday";
  * Retrieves the completion status of a quest for the current user (client-side).
  *
  * @param questId - The ID of the quest to check
- * @param userID - Optional user ID. If not provided, the current authenticated user is used
  * @returns A promise that resolves to `true` if the quest was completed today, `false` otherwise
  * @throws Will throw an error if there's an issue fetching quest completion data or if the user is not authenticated
  */
 export default async function getQuestCompletionStatus(
   questId: number,
-  userID?: string,
 ): Promise<boolean> {
   const supabase = createSupabaseBrowserClient();
 
@@ -27,16 +25,13 @@ export default async function getQuestCompletionStatus(
     throw new Error("Error fetching quest completion status: " + error.message);
   }
 
-  let userId = userID;
-  if (!userId) {
-    const { data: userData, error: userError } = await supabase.auth.getUser();
-    if (userError || !userData.user) {
-      console.error("Error fetching user ID:", userError);
-      throw new Error("User not authenticated");
-    }
-    userId = userData.user.id;
+  const { data: userData, error: userError } = await supabase.auth.getUser();
+  if (userError || !userData.user) {
+    console.error("Error fetching user ID:", userError);
+    throw new Error("User not authenticated");
   }
-
+  
+  const userId = userData.user.id;
   const isCompletedToday = await isQuestCompletedToday({
     userId,
     latestCompletion: data[0] ?? null,
