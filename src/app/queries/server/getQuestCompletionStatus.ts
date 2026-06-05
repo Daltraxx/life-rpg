e40ba2b90e5fb8acfb004283c858d3909761dcdb
Dalton Pettus <daltpettus@gmail.com>
2026-06-04 17:23:45 -0700

    documentation

--- src/app/queries/server/getQuestCompletionStatus.ts
+++ src/app/queries/server/getQuestCompletionStatus.ts
@@ -7,7 +7,7 @@ import isQuestCompletedToday from "./helpers/isQuestCompletedToday";
  * @param questId - The ID of the quest to check
  * @param userId - The ID of the authenticated user
  * @returns A promise that resolves to `true` if the quest was completed today, `false` otherwise
- * @throws Will throw an error if there's an issue fetching quest completion data or if the user is not authenticated
+ * @throws Will throw an error if there's an issue fetching quest completion data from the database
  */
 export default async function getQuestCompletionStatus(
   questId: number,
