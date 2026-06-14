3877088415831148b967e61483c89373cdb67dd2
Dalton Pettus <daltpettus@gmail.com>
2026-06-14 14:41:53 -0700

    create edit-profile route

---
+++ src/app/(main)/edit-profile/page.tsx
@@ -0,0 +1,12 @@
+import { type Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Edit Profile",
+  description:
+    "Edit your LifeRPG profile, changing your quests and attributes.",
+};
+
+export default function ProfilePage() {
+  return (
+    <div>Edit profile page</div>
+  );
+}
