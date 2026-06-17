d675dc2063ee6d2f10a4fad1c16fa7e2503eb61a
Dalton Pettus <daltpettus@gmail.com>
2026-06-16 21:01:17 -0700

    get profile completion status in header and pass to OptionsMenu

--- src/app/ui/layout/Header/Header.tsx
+++ src/app/ui/layout/Header/Header.tsx
@@ -5,20 +5,41 @@ import styles from "./styles.module.css";
 import { JSX } from "react";
 import OptionsMenu from "./OptionsMenu/OptionsMenu";
 import { ROUTES } from "@/utils/constants/routes";
+import { getProfileComplete } from "./helpers/get-profile-complete";
 
-export default function Header(): JSX.Element {
-  return (
-    <Bounded
-      as="header"
-      outerClassName={styles.boundedContainer}
-      innerClassName={styles.contentContainer}
-    >
-      <Link href={ROUTES.HOME} className={styles.brandingLink}>
+export default async function Header(): Promise<JSX.Element> {
+  try {
+    const profileComplete = await getProfileComplete();
+    return (
+      <Bounded
+        as="header"
+        outerClassName={styles.boundedContainer}
+        innerClassName={styles.contentContainer}
+      >
+        <Link href={ROUTES.HOME} className={styles.brandingLink}>
+          <Heading as="h2" size="72" className={styles.branding}>
+            LifeRPG
+          </Heading>
+        </Link>
+        <OptionsMenu
+          className={styles.options}
+          profileComplete={profileComplete}
+        />
+      </Bounded>
+    );
+  } catch (error) {
+    console.error("Error rendering header:", { cause: error });
+    // TODO: create error page and route, then redirect to it here instead of rendering fallback UI
+    return (
+      <Bounded
+        as="header"
+        outerClassName={styles.boundedContainer}
+        innerClassName={styles.contentContainer}
+      >
         <Heading as="h2" size="72" className={styles.branding}>
           LifeRPG
         </Heading>
-      </Link>
-      <OptionsMenu className={styles.options} />
-    </Bounded>
-  );
+      </Bounded>
+    );
+  }
 }
