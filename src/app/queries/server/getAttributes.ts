0a84aab7db7a18c491865af7bd3403205d7fd38f
Dalton Pettus <daltpettus@gmail.com>
2026-06-18 10:22:04 -0700

    update doc

--- src/app/queries/server/getAttributes.ts
+++ src/app/queries/server/getAttributes.ts
@@ -6,7 +6,7 @@ import { AttributeArraySchema } from "@/utils/validations/attribute";
  * Fetches all attributes for the currently authenticated user.
  *
  * @returns {Promise<Attribute[]>} A promise that resolves to an array of attributes ordered by position.
- * @throws {Error} Throws an error if the user is not authenticated or if the database query fails.
+ * @throws {Error} Throws an error if the user is not authenticated, the database query fails, or the attribute data is invalid.
  */
 export default async function getAttributes(): Promise<Attribute[]> {
   const supabase = await createSupabaseServerClient();
