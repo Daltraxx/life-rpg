c7cf98c9aa2472910a3e7854cc72764a6a7d8ff4
Dalton Pettus <daltpettus@gmail.com>
2026-06-17 19:45:55 -0700

    pass queried quests and attributes to setup ui from edit profile page

--- src/app/(main)/edit-profile/page.tsx
+++ src/app/(main)/edit-profile/page.tsx
@@ -1,15 +1,20 @@
 import { type Metadata } from "next";
-import EditUI from "@/app/ui/profile-editor/EditUI";
+import SetupUI from "@/app/ui/profile-editor/SetupUI";
+import getQuests from "@/app/queries/server/getQuests";
+import getAttributes from "@/app/queries/server/getAttributes";
+
 export const metadata: Metadata = {
   title: "Edit Profile",
   description:
     "Edit your LifeRPG profile, changing your quests and attributes.",
 };
 
-export default function EditProfilePage() {
+export default async function EditProfilePage() {
+  const quests = await getQuests();
+  const attributes = await getAttributes();
   return (
     <>
-      <EditUI />
+      <SetupUI initialQuests={quests} initialAttributes={attributes} />
     </>
   );
 }
