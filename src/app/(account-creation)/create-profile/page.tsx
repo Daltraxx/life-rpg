64798b0940a0c4df5a843d32aec05dfc666b010a
Dalton Pettus <daltpettus@gmail.com>
2026-06-17 19:57:45 -0700

    pass setup attributes and quests to setup ui from account setup page

--- src/app/(account-creation)/create-profile/page.tsx
+++ src/app/(account-creation)/create-profile/page.tsx
@@ -4,12 +4,21 @@ import { createSupabaseServerClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import SetupUI from "@/app/ui/profile-editor/SetupUI";
 import { ROUTES } from "@/utils/constants/routes";
+import { Attribute } from "@/utils/types/attribute";
 
 export const metadata: Metadata = {
   title: "Account Setup",
   description:
     "Set up your account to start progressing with Life RPG. Choose the attributes you wish to level, and the daily quests that will get you there.",
 };
+
+const INITIAL_ATTRIBUTES: Attribute[] = [
+  { id: crypto.randomUUID(), name: "Discipline", experience: 0, level: 1, position: 1 },
+  { id: crypto.randomUUID(), name: "Vitality", experience: 0, level: 1, position: 2 },
+  { id: crypto.randomUUID(), name: "Intelligence", experience: 0, level: 1, position: 3 },
+  { id: crypto.randomUUID(), name: "Fitness", experience: 0, level: 1, position: 4 },
+];
+
 export default async function AccountSetupPage() {
   // TODO: add caching for user profile data to limit database requests
   const supabase = await createSupabaseServerClient();
@@ -30,7 +39,7 @@ export default async function AccountSetupPage() {
   return (
     <>
       <Intro authUser={user} />
-      <SetupUI />
+      <SetupUI initialQuests={[]} initialAttributes={INITIAL_ATTRIBUTES} />
     </>
   );
 }
