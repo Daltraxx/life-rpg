fb5ed5ffe27737a433e01df65ad72d81107a84d2
Dalton Pettus <daltpettus@gmail.com>
2026-06-17 19:58:27 -0700

    allow string for attribute id for pre-db creation

--- src/utils/types/attribute.ts
+++ src/utils/types/attribute.ts
@@ -1,13 +1,14 @@
 /**
  * Represents an attribute for a user.
- * @property {number} id - A unique identifier for the attribute.
+ * @property {number | string} id - A unique identifier for the attribute. 
+ * Can be either a number (for database IDs) or a string (for temporary or client-side IDs).
  * @property {string} name - The name of the attribute (e.g., "Strength", "Intelligence").
  * @property {number} experience - The total experience points accumulated for this attribute.
  * @property {number} level - The current level of the attribute based on the accumulated experience.
  * @property {number} position - The order in which the attribute is displayed in the UI.
  */
 export interface Attribute {
-  id: number;
+  id: number | string;
   name: string;
   experience: number;
   level: number;
