6835e56f8e69b7e6a5ebe519380e5212c9dcaa60
Dalton Pettus <daltpettus@gmail.com>
2026-06-19 13:29:23 -0700

    create profile edit schema

---
+++ src/utils/validations/profile-edit/profile-edit.ts
@@ -0,0 +1,63 @@
+import { z } from "zod";
+import { TransactionQuestSchema } from "@/utils/validations/profile-edit/transaction-quest";
+import { TransactionAttributeSchema } from "@/utils/validations/profile-edit/transaction-attribute";
+import hasUniqueValues from "@/utils/helpers/hasUniqueValues";
+import {
+  MAX_QUESTS_ALLOWED,
+  MIN_QUESTS_ALLOWED,
+  MAX_ATTRIBUTES_ALLOWED,
+  MIN_ATTRIBUTES_ALLOWED,
+} from "@/utils/constants/gameConstants";
+import { getNounAndVerbAgreement } from "@/utils/helpers/pluralOrSingularHandlers";
+
+export const ProfileEditSchema = z.object({
+  quests: z
+    .array(TransactionQuestSchema)
+    .min(
+      MIN_QUESTS_ALLOWED,
+      `At least ${MIN_QUESTS_ALLOWED} ${getNounAndVerbAgreement(
+        "quest",
+        MIN_QUESTS_ALLOWED,
+      )} required`,
+    )
+    .max(
+      MAX_QUESTS_ALLOWED,
+      `No more than ${MAX_QUESTS_ALLOWED} ${getNounAndVerbAgreement(
+        "quest",
+        MAX_QUESTS_ALLOWED,
+      )} allowed`,
+    )
+    .refine((quests) => hasUniqueValues(quests, "name"), {
+      message: "Quest names must be unique",
+    }),
+  attributes: z
+    .array(TransactionAttributeSchema)
+    .min(
+      MIN_ATTRIBUTES_ALLOWED,
+      `At least ${MIN_ATTRIBUTES_ALLOWED} ${getNounAndVerbAgreement(
+        "attribute",
+        MIN_ATTRIBUTES_ALLOWED,
+      )} required`,
+    )
+    .max(
+      MAX_ATTRIBUTES_ALLOWED,
+      `No more than ${MAX_ATTRIBUTES_ALLOWED} ${getNounAndVerbAgreement(
+        "attribute",
+        MAX_ATTRIBUTES_ALLOWED,
+      )} allowed`,
+    )
+    .refine((attributes) => hasUniqueValues(attributes, "name"), {
+      message: "Attribute names must be unique",
+    }),
+});
+
+export type ProfileEditFormData = z.infer<typeof ProfileEditSchema>;
+
+export type ProfileEditState = {
+  errors?: {
+    userId?: string[];
+    quests?: string[];
+    attributes?: string[];
+  };
+  message?: string;
+};
