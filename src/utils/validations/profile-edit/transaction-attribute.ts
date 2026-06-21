136eab92d6763680e8b36769b5ccac74bc3bdb11
Dalton Pettus <daltpettus@gmail.com>
2026-06-20 17:28:38 -0700

    update edit transaction attribute to allow string or int id

--- src/utils/validations/profile-edit/transaction-attribute.ts
+++ src/utils/validations/profile-edit/transaction-attribute.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 import { TransactionAttributeSchema as ProfileCreationAttributeSchema } from "@/utils/validations/profile-creation/transaction-attribute";
 
 export const TransactionAttributeSchema = ProfileCreationAttributeSchema.extend({
-  id: z.string().optional(),
+  id: z.union([z.number(), z.string()]), // Allow both existing attributes with numeric IDs and new attributes with string client keys
   experience: z.number().nonnegative("Experience must be a non-negative number"),
   level: z.number().nonnegative("Level must be a non-negative number"),
   position: z.number().nonnegative("Position must be a non-negative number"),
