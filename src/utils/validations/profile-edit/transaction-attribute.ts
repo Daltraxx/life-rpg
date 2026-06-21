941dc4f6e0529e890cb05611bf766aa61b64645b
Dalton Pettus <daltpettus@gmail.com>
2026-06-21 12:58:37 -0700

    use int not number in zod validation

--- src/utils/validations/profile-edit/transaction-attribute.ts
+++ src/utils/validations/profile-edit/transaction-attribute.ts
@@ -2,10 +2,10 @@ import { z } from "zod";
 import { TransactionAttributeSchema as ProfileCreationAttributeSchema } from "@/utils/validations/profile-creation/transaction-attribute";
 
 export const TransactionAttributeSchema = ProfileCreationAttributeSchema.extend({
-  id: z.union([z.number(), z.string()]), // Allow both existing attributes with numeric IDs and new attributes with string client keys
-  experience: z.number().nonnegative("Experience must be a non-negative number"),
-  level: z.number().nonnegative("Level must be a non-negative number"),
-  position: z.number().nonnegative("Position must be a non-negative number"),
+  id: z.union([z.int(), z.string()]), // Allow both existing attributes with numeric IDs and new attributes with string client keys
+  experience: z.int().nonnegative("Experience must be a non-negative number"),
+  level: z.int().nonnegative("Level must be a non-negative number"),
+  position: z.int().nonnegative("Position must be a non-negative number"),
 });
 
 export type TransactionAttribute = z.infer<typeof TransactionAttributeSchema>;
