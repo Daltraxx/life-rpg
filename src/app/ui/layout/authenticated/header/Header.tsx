7f8077c35c784d490dc90b93d355d4972db80a7f
Dalton Pettus <daltpettus@gmail.com>
2026-06-24 22:10:46 -0700

    refactor authenticated Header component

--- src/app/ui/layout/authenticated/header/Header.tsx
+++ src/app/ui/layout/authenticated/header/Header.tsx
@@ -5,11 +5,27 @@ import styles from "./styles.module.css";
 import { JSX } from "react";
 import OptionsMenu from "./OptionsMenu/OptionsMenu";
 import { ROUTES } from "@/utils/constants/routes";
-import { getProfileComplete } from "./helpers/get-profile-complete";
+import { getResolvedProfileCompletionStatus } from "@/utils/helpers/get-resolved-profile-completion-status";
 
 export default async function Header(): Promise<JSX.Element> {
-  try {
-    const profileComplete = await getProfileComplete();
+  const { data: profileComplete, error } = await getResolvedProfileCompletionStatus();
+  if (error) {
+    console.error("Error rendering header:", { error });
+    // TODO: create error page and route, then redirect to it here instead of rendering fallback UI
+    return (
+      <Bounded
+        as="header"
+        outerClassName={styles.boundedContainer}
+        innerClassName={styles.contentContainer}
+      >
+        <Link href={ROUTES.HOME} className={styles.brandingLink}>
+          <Heading as="h2" size="72" className={styles.branding}>
+            LifeRPG
+          </Heading>
+        </Link>
+      </Bounded>
+    );
+  }
     return (
       <Bounded
         as="header"
@@ -27,21 +43,4 @@ export default async function Header(): Promise<JSX.Element> {
         />
       </Bounded>
     );
-  } catch (error) {
-    console.error("Error rendering header:", { error });
-    // TODO: create error page and route, then redirect to it here instead of rendering fallback UI
-    return (
-      <Bounded
-        as="header"
-        outerClassName={styles.boundedContainer}
-        innerClassName={styles.contentContainer}
-      >
-        <Link href={ROUTES.HOME} className={styles.brandingLink}>
-          <Heading as="h2" size="72" className={styles.branding}>
-            LifeRPG
-          </Heading>
-        </Link>
-      </Bounded>
-    );
-  }
 }
