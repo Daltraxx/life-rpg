a2d50d04fae205fa5cb261671aba3a35d277583c
Dalton Pettus <daltpettus@gmail.com>
2026-06-25 21:14:28 -0700

    remove unused error var

--- src/utils/validations/timezone.ts
+++ src/utils/validations/timezone.ts
@@ -8,7 +8,7 @@ export const isValidTimezone = (timezone: string): boolean => {
   try {
     Intl.DateTimeFormat(undefined, { timeZone: timezone });
     return true;
-  } catch (error) {
+  } catch {
     return false;
   }
 };
